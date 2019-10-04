8dcdee76bac994b22e1b56b01da07e8ea28d76af
Matthieu Barba <mbarba@ebi.ac.uk>
2019-10-04 17:04:00 +0100

    Add seqregion dumping runnable

---
+++ lib/Bio/EnsEMBL/Pipeline/Runnable/DumpSeqRegionJson.pm
@@ -0,0 +1,74 @@
+package Bio::EnsEMBL::Pipeline::Runnable::DumpSeqRegionJson;
+
+use strict;
+use warnings;
+
+use JSON;
+use File::Path qw(make_path);
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
+use base ('Bio::EnsEMBL::Production::Pipeline::Common::Base');
+
+sub fetch_input {
+  my ($self) = @_;
+
+  $self->param( 'dba', $self->core_dba() );
+
+  return;
+}
+
+sub param_defaults {
+  return {};
+}
+
+sub run {
+  my ($self) = @_;
+
+  print $self->param('species');
+  $self->write_json();
+  return;
+}
+
+sub write_json {
+  my ($self) = @_;
+
+  my $sub_dir = $self->create_dir('json');
+  $self->info(
+          "Processing " . $self->production_name() . " into $sub_dir" );
+  my $dba = $self->core_dba();
+
+  # Get seq_regions
+  my $sa = Bio::EnsEMBL::Registry->get_adaptor(
+    $self->production_name, "core", "slice" );
+
+  # Get all top level seq regions
+  my $slices = $sa->fetch_all('toplevel');
+
+  my @seq_regions;
+  foreach my $slice (@$slices) {
+    my $seq_region = {
+      name => $slice->seq_region_name(),
+    };
+    push @seq_regions, $seq_region;
+  }
+
+  $dba->dbc()->disconnect_if_idle();
+
+  # Write data to json
+  $self->write_json_file($sub_dir, \@seq_regions);
+} ## end sub write_json
+
+sub write_json_file {
+  my ($self, $sub_dir, $data) = @_;
+  my $json_file_path =
+    $sub_dir . '/' . $self->production_name() . '.json';
+  $self->info("Writing to $json_file_path");
+  open my $json_file, '>', $json_file_path or
+    die "Could not open $json_file_path for writing";
+  print $json_file encode_json($data);
+  close $json_file;
+  $self->info("Write complete");
+  return;
+}
+
+1;
