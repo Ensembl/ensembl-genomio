217cb1e6e4c6e69e97ccf10998b79400d58af0c4
Matthieu Barba <mbarba@ebi.ac.uk>
2019-10-09 15:18:49 +0100

    Add base BRC4 dumper conf based on Ensembl prod dumper

---
+++ lib/Bio/EnsEMBL/Pipeline/PipeConfig/BRC4_genome_dumper_conf.pm
@@ -0,0 +1,271 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2019] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+ Bio::EnsEMBL::Pipeline::PipeConfig::BRC4_genome_dumper_conf;
+
+=head1 DESCRIPTION
+
+=head1 AUTHORS
+
+ ckong@ebi.ac.uk
+ mbarba@ebi.ac.uk
+
+=cut
+package Bio::EnsEMBL::Pipeline::PipeConfig::BRC4_genome_dumper_conf;
+
+use strict;
+use warnings;
+use File::Spec;
+use Data::Dumper;
+use Bio::EnsEMBL::Hive::Version 2.4;
+use Bio::EnsEMBL::ApiVersion qw/software_version/;
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+use base ('Bio::EnsEMBL::Hive::PipeConfig::EnsemblGeneric_conf');  
+   
+sub default_options {
+	my ($self) = @_;
+	return {
+       ## inherit other stuff from the base class
+       %{ $self->SUPER::default_options() },
+
+	   ## General parameters
+       'registry'      => $self->o('registry'),   
+       'release'       => $self->o('release'),
+       'pipeline_name' => "brc4_genome_dumper",
+	     'email'         => $self->o('ENV', 'USER').'@ebi.ac.uk',
+       'output_dir'    => './output',
+
+	   ## 'job_factory' parameters
+	   'species'     => [], 
+	   'antispecies' => [],
+       'division' 	 => [], 
+	   'run_all'     => 0,	
+
+	   ## gff3 & gtf parameter
+       'abinitio'        => 1,
+       'gene' => 1,
+
+       ## gff3 parameters
+       'gt_exe'          => 'gt',
+       'gff3_tidy'       => $self->o('gt_exe').' gff3 -tidy -sort -retainids -force',
+       'gff3_validate'   => $self->o('gt_exe').' gff3validator',
+
+       'feature_type'    => ['Gene', 'Transcript', 'SimpleFeature'], #'RepeatFeature'
+       'per_chromosome'  => 1,
+       'include_scaffold'=> 1,
+	   'logic_name'      => [],
+	   'db_type'	     => 'core',
+       'out_file_stem'   => undef,
+       'xrefs'           => 0,
+
+	   ## fasta parameters
+       # types to emit
+       'dna_sequence_type_list'  => ['dna'],
+       'pep_sequence_type_list'  => ['cdna', 'ncrna'],
+
+       # Do/Don't process these logic names
+       'process_logic_names' => [],
+       'skip_logic_names'    => [],
+	};
+}
+
+sub pipeline_create_commands {
+    my ($self) = @_;
+    return [
+      # inheriting database and hive tables' creation
+      @{$self->SUPER::pipeline_create_commands},
+      'mkdir -p '.$self->o('output_dir'),
+    ];
+}
+
+# Ensures output parameters get propagated implicitly
+sub hive_meta_table {
+  my ($self) = @_;
+  
+  return {
+    %{$self->SUPER::hive_meta_table},
+    'hive_use_param_stack'  => 1,
+  };
+}
+
+# Override the default method, to force an automatic loading of the registry in all workers
+sub beekeeper_extra_cmdline_options {
+  my ($self) = @_;
+  return 
+      ' -reg_conf ' . $self->o('registry'),
+  ;
+}
+
+# these parameter values are visible to all analyses, 
+# can be overridden by parameters{} and input_id{}
+sub pipeline_wide_parameters {  
+    my ($self) = @_;
+    return {
+            %{$self->SUPER::pipeline_wide_parameters},  # here we inherit anything from the base class
+		    'pipeline_name' => $self->o('pipeline_name'), #This must be defined for the beekeeper to work properly
+            'base_path'     => $self->o('output_dir'),
+            'release'       => $self->o('release'),
+    };
+}
+
+sub resource_classes {
+    my $self = shift;
+    return {
+      'default'  	=> {'LSF' => '-q production-rh74 -M 4000   -R "rusage[mem=4000]"'},
+      '8GB'       => {'LSF' => '-q production-rh74 -M 8000   -R "rusage[mem=8000]"'},
+      '15GB'      => {'LSF' => '-q production-rh74 -M 15000  -R "rusage[mem=15000]"'},
+      '32GB'  	 	=> {'LSF' => '-q production-rh74 -M 32000  -R "rusage[mem=32000]"'},
+      '64GB'  	 	=> {'LSF' => '-q production-rh74 -M 64000  -R "rusage[mem=64000]"'},
+      '128GB'  	 	=> {'LSF' => '-q production-rh74 -M 128000 -R "rusage[mem=128000]"'},
+      '256GB'  	 	=> {'LSF' => '-q production-rh74 -M 256000 -R "rusage[mem=256000]"'},
+	}
+}
+
+sub pipeline_analyses {
+    my ($self) = @_;
+    
+    return [
+	 { -logic_name     => 'Species_factory',
+       -module         => 'Bio::EnsEMBL::Production::Pipeline::Common::SpeciesFactory',
+       -parameters     => {
+                             species     => $self->o('species'),
+                             antispecies => $self->o('antispecies'),
+                             division    => $self->o('division'),
+                             run_all     => $self->o('run_all'),
+                          },
+      -input_ids      => [ {} ],
+	    -hive_capacity   => -1,
+      -rc_name 	       => 'default',
+      -max_retry_count => 1,
+      -flow_into       => { '2' => 'backbone_job_pipeline', },
+    },
+
+     { -logic_name     => 'backbone_job_pipeline',
+       -module         => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+       -hive_capacity  => -1,
+       -rc_name 	   => 'default',
+       -flow_into      => {'1->A' => ['fasta_dna', 'fasta_pep', 'gff3'],
+                           'A->1' => ['cleanup'],
+                          }
+     },
+    {  -logic_name => 'cleanup',
+       -module         => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+       -hive_capacity => 10,
+       -rc_name       => 'default'
+    },
+ 	
+### GFF3
+     { -logic_name     => 'gff3',
+       -module         => 'Bio::EnsEMBL::Production::Pipeline::GFF3::DumpFile',
+       -parameters     => {
+          feature_type       => $self->o('feature_type'),
+          per_chromosome     => $self->o('per_chromosome'),
+          include_scaffold   => $self->o('include_scaffold'),
+          logic_name         => $self->o('logic_name'),
+          db_type            => $self->o('db_type'),
+	      abinitio           => $self->o('abinitio'),
+        gene               => $self->o('gene'),
+	      out_file_stem      => $self->o('out_file_stem'),
+	      xrefs              => $self->o('xrefs'),        
+        },
+       -hive_capacity  => 50,
+       -rc_name 	   => 'default',
+       -flow_into      => { '-1' => 'gff3_highmem', '1'  => 'tidy_gff3' },
+     },
+
+	 { -logic_name     => 'gff3_highmem',
+       -module         => 'Bio::EnsEMBL::Production::Pipeline::GFF3::DumpFile',
+       -parameters     => {
+          feature_type       => $self->o('feature_type'),
+          per_chromosome     => $self->o('per_chromosome'),
+          include_scaffold   => $self->o('include_scaffold'),
+          logic_name         => $self->o('logic_name'),
+          db_type            => $self->o('db_type'),
+    	  abinitio           => $self->o('abinitio'),
+        gene               => $self->o('gene'),
+	      out_file_stem      => $self->o('out_file_stem'),
+	      xrefs              => $self->o('xrefs'),        
+        },
+	    -hive_capacity  => 50,
+      -rc_name        => '32GB',
+      -flow_into      => { '1'  => 'tidy_gff3' },
+	 },	
+
+### GFF3:post-processing
+     { -logic_name     => 'tidy_gff3',
+       -module         => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+       -parameters     => {  cmd => $self->o('gff3_tidy').' -gzip -o #out_file#.sorted.gz #out_file#', },
+       -hive_capacity  => 10,
+       -batch_size     => 10,
+	     -rc_name        => 'default',
+       -flow_into      => 'move_gff3',
+     },
+
+     {
+       -logic_name     => 'move_gff3',
+       -module         => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+       -parameters     => { cmd => 'mv #out_file#.sorted.gz #out_file#', },
+       -hive_capacity  => 10,
+       -rc_name        => 'default',
+       -flow_into      => 'validate_gff3',
+      },
+ 
+      {
+     	-logic_name        => 'validate_gff3',
+     	-module            => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+     	-parameters        => { cmd => $self->o('gff3_validate').' #out_file#', },
+     	-hive_capacity => 10,
+     	-batch_size        => 10,
+     	-rc_name           => 'default',
+   	  },
+
+
+### FASTA (cdna, cds, dna, pep, ncrna)
+    { -logic_name  => 'fasta_pep',
+      -module      => 'Bio::EnsEMBL::Production::Pipeline::FASTA::DumpFile',
+      -parameters  => {
+            sequence_type_list  => $self->o('pep_sequence_type_list'),
+          	process_logic_names => $self->o('process_logic_names'),
+          	skip_logic_names    => $self->o('skip_logic_names'),
+       },
+      -can_be_empty    => 1,
+      -max_retry_count => 1,
+      -hive_capacity   => 20,
+      -priority        => 5,
+      -rc_name         => 'default',
+    },
+    { -logic_name  => 'fasta_dna',
+      -module      => 'Bio::EnsEMBL::Production::Pipeline::FASTA::DumpFile',
+      -parameters  => {
+            sequence_type_list  => $self->o('dna_sequence_type_list'),
+            process_logic_names => $self->o('process_logic_names'),
+            skip_logic_names    => $self->o('skip_logic_names'),
+       },
+      -can_be_empty    => 1,
+      -max_retry_count => 1,
+      -hive_capacity   => 20,
+      -priority        => 5,
+      -rc_name         => 'default',
+    },
+
+    ];
+}
+
+1;
+
