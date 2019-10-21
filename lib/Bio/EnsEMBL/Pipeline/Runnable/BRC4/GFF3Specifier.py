ba322de3e126fb0d7eb78e9528bdb62cfe635b39
Matthieu Barba <mbarba@ebi.ac.uk>
2019-10-21 16:41:25 +0100

    Use plain text or gzip depending on gff3 file extension

--- lib/Bio/EnsEMBL/Pipeline/Runnable/BRC4/GFF3Specifier.py
+++ lib/Bio/EnsEMBL/Pipeline/Runnable/BRC4/GFF3Specifier.py
@@ -36,61 +36,67 @@ class GFF3Specifier(eHive.BaseRunnable):
 
     def get_gff3(self, gff3_path):
 
-        seqs = {};
-        genes = {};
-        peps = {};
+        gff3_tmp_path = gff3_path + ".tmp"
 
-        gff3_tmp_path = gff3_path + ".tmp.gz"
+        if gff3_path.endswith(".gz"):
+            gff3_tmp_path += ".gz"
+            with io.TextIOWrapper(gzip.open(gff3_path, "r")) as gff3_handle:
+                with io.TextIOWrapper(gzip.open(gff3_tmp_path, "w")) as tmph:
+                    self.parse_gff3(gff3_handle, tmph)
+        else:
+            with open(gff3_path, "r") as gff3_handle:
+                with open(gff3_tmp_path, "w") as tmph:
+                    self.parse_gff3(gff3_handle, tmph)
+
+
+        return gff3_tmp_path
+
+    def parse_gff3(self, gff3_handle, tmph):
 
         skipped_biotypes = {}
         skipped_attributes = {}
+        for line in gff3_handle:
+            # Skip separator lines
+            if line[0:3] == "###":
+                continue
+            # Keep all comment lines intact
+            elif line[0] == "#":
+                tmph.write(line)
+            else:
+                line = line.rstrip()
+                (chrom, source, biotype, start, end, col6, col7, col8, attribs) = line.split("\t")
 
-        with io.TextIOWrapper(gzip.open(gff3_path, "r")) as gff3_handle:
-            with io.TextIOWrapper(gzip.open(gff3_tmp_path, "w")) as tmph:
-
-                for line in gff3_handle:
-                    # Skip separator lines
-                    if line[0:3] == "###":
+                # Filter biotype
+                if self.allowed_biotypes and biotype not in self.allowed_biotypes:
+                    if not (biotype.endswith("RNA") or biotype.endswith("RNA_gene")):
+                        if biotype not in skipped_biotypes:
+                            skipped_biotypes[biotype] = 0
+                        skipped_biotypes[biotype] += 1
                         continue
-                    # Keep all comment lines intact
-                    elif line[0] == "#":
-                        tmph.write(line)
-                    else:
-                        line = line.rstrip()
-                        (chrom, source, biotype, start, end, col6, col7, col8, attribs) = line.split("\t")
 
-                        # Filter biotype
-                        if self.allowed_biotypes and biotype not in self.allowed_biotypes:
-                            if not (biotype.endswith("RNA") or biotype.endswith("RNA_gene")):
-                                if biotype not in skipped_biotypes:
-                                    skipped_biotypes[biotype] = 0
-                                skipped_biotypes[biotype] += 1
-                                continue
+                # Filter attributes
+                attrs = attribs.split(";")
+                attributes = []
+                for a in attrs:
+                    (key, value) = a.split("=")
+                    if self.allowed_attributes and key not in self.allowed_attributes:
+                        if not key in skipped_attributes:
+                            skipped_attributes[key] = 0
+                        skipped_attributes[key] += 1
+                        continue
 
-                        # Filter attributes
-                        attrs = attribs.split(";")
-                        attributes = []
-                        for a in attrs:
-                            (key, value) = a.split("=")
-                            if self.allowed_attributes and key not in self.allowed_attributes:
-                                if not key in skipped_attributes:
-                                    skipped_attributes[key] = 0
-                                skipped_attributes[key] += 1
-                                continue
+                    # Special production-specific modification
+                    if key == "ID" and ":" in value:
+                        (btype, real_value) = value.split(":")
+                        if btype and real_value:
+                            value = real_value
 
-                            # Special production-specific modification
-                            if key == "ID":
-                                (btype, real_value) = value.split(":")
-                                value = real_value
+                    attributes.append(key + ":" + value)
 
-                            attributes.append(key + ":" + value)
-
-                        # Print back the line
-                        new_attribs = ";".join(attributes)
-                        new_line = "\t".join([chrom, source, biotype, start, end, col6, col7, col8, new_attribs]) + "\n"
-                        tmph.write(new_line)
-
-                        #raise Exception("die")
+                # Print back the line
+                new_attribs = ";".join(attributes)
+                new_line = "\t".join([chrom, source, biotype, start, end, col6, col7, col8, new_attribs]) + "\n"
+                tmph.write(new_line)
 
         if skipped_biotypes:
             print("Skipped biotypes:")
@@ -101,5 +107,3 @@ class GFF3Specifier(eHive.BaseRunnable):
             for at in skipped_attributes:
                 print("\t%s: %d" % (at, skipped_attributes[at]))
 
-
-        return gff3_tmp_path
