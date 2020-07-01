2bdaddc0c02857f5757d8d5f330376c1e786d096
Vasily Sitnik <vsitnik@ebi.ac.uk>
2020-07-01 23:39:40 +0100

    JSON rule to keep scalar/vector leaf value during interpolation

--- scripts/gff_metaparser/gffstruct/rules/json.py
+++ scripts/gff_metaparser/gffstruct/rules/json.py
@@ -182,7 +182,7 @@ class JsonRule(BaseRule):
       return out, interpolated
 
     # scalar section
-    v = context.get(data, default = data)
+    v = context.get(data, data)
 
     asplit = self._actions["split"]
     asub = self._actions["sub"]
@@ -242,6 +242,8 @@ class JsonRule(BaseRule):
     if type(value) == list and _a["key_is_list"] == False:
       # force scalar
       value = ",".join(value)
+    context_data = context.data.copy()
+    context_data["_LEAFVALUE"] = value
 
     itag = None
     data = {}
@@ -253,7 +255,7 @@ class JsonRule(BaseRule):
     if _a["addon"]:
       data.update(_a["addon"])
 
-    data, _ = self.interpolate(data, context)
+    data, _ = self.interpolate(data, context_data)
     if data is None:
       return
 
