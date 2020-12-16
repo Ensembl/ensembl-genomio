dac3bd2b1666bbbd51ac8598b60d92e7f76f0c25
Matthieu Barba <mbarba@ebi.ac.uk>
2020-12-16 15:14:21 +0000

    Also copy the batch version of LoadGFF3

---
+++ lib/perl/Bio/EnsEMBL/Pipeline/PipeConfig/LoadGFF3Batch_conf.pm
@@ -0,0 +1,125 @@
+=head1 LICENSE
+
+Copyright [1999-2014] EMBL-European Bioinformatics Institute
+and Wellcome Trust Sanger Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+
+=pod
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::Runnable::EG::PipeConfig::LoadGFF3Batch_conf
+
+=head1 DESCRIPTION
+
+Load valid GFF3 files into core databases.
+
+=head1 Author
+
+James Allen
+
+=cut
+
+package Bio::EnsEMBL::Pipeline::PipeConfig::LoadGFF3Batch_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Pipeline::PipeConfig::LoadGFF3_conf');
+
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+use Bio::EnsEMBL::Hive::Version 2.4;
+
+sub default_options {
+  my ($self) = @_;
+  return {
+    %{$self->default_options_generic},
+    
+    pipeline_name => 'load_gff3_batch',
+    
+    species      => [],
+    division     => [],
+    run_all      => 0,
+    antispecies  => [],
+    meta_filters => {},
+    
+    # The code assumes that for all species there will be an appropriately
+    # named subdirectory in the 'source_dir', and within that subdir the
+    # GFF3 files will have the same name. Ditto other file types.
+    gff3_tidy_filename     => $self->o('gff3_filename').'.tidied',
+    fasta_filename         => undef,
+    genbank_filename       => undef,
+    protein_fasta_filename => undef,
+    
+    # Since this mode was designed to be used with patch builds,
+    # default to not deleting any existing genes.
+    delete_existing => 0,
+  };
+}
+
+sub pipeline_wide_parameters {
+  my ($self) = @_;
+  
+  return $self->pipeline_wide_parameters_generic;
+}
+
+sub pipeline_analyses {
+  my ($self) = @_;
+  
+  return [
+    {
+      -logic_name      => 'SpeciesFactory',
+      -module          => 'Bio::EnsEMBL::Pipeline::Runnable::EG::Common::RunnableDB::EGSpeciesFactory',
+      -input_ids       => [ {} ],
+      -parameters      => {
+                            species         => $self->o('species'),
+                            antispecies     => $self->o('antispecies'),
+                            division        => $self->o('division'),
+                            run_all         => $self->o('run_all'),
+                            meta_filters    => $self->o('meta_filters'),
+                            chromosome_flow => 0,
+                            regulation_flow => 0,
+                            variation_flow  => 0,
+                          },
+      -max_retry_count => 1,
+      -flow_into       => {
+                            2 => ['ConstructFilenames'],
+                          },
+      -meadow_type     => 'LOCAL',
+    },
+
+    {
+      -logic_name      => 'ConstructFilenames',
+      -module          => 'Bio::EnsEMBL::Pipeline::Runnable::EG::LoadGFF3::ConstructFilenames',
+      -max_retry_count => 0,
+      -parameters      => {
+                            source_dir             => $self->o('source_dir'),
+                            gff3_filename          => $self->o('gff3_filename'),
+                            gff3_tidy_filename     => $self->o('gff3_tidy_filename'),
+                            fasta_filename         => $self->o('fasta_filename'),
+                            genbank_filename       => $self->o('genbank_filename'),
+                            protein_fasta_filename => $self->o('protein_fasta_filename'),
+                          },
+      -flow_into       => ['GFF3Tidy'],
+      -meadow_type     => 'LOCAL',
+    },
+    
+    @{ $self->pipeline_analyses_generic}
+  ];
+}
+
+1;
---
+++ lib/perl/Bio/EnsEMBL/Pipeline/Runnable/EG/LoadGFF3/ConstructFilenames.pm
@@ -0,0 +1,74 @@
+=head1 LICENSE
+
+Copyright [1999-2014] EMBL-European Bioinformatics Institute
+and Wellcome Trust Sanger Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+
+=pod
+
+=head1 NAME
+
+Bio::EnsEMBL::Pipeline::Runnable::EG::LoadGFF3::ConstructFilenames
+
+=head1 Author
+
+James Allen
+
+=cut
+
+package Bio::EnsEMBL::Pipeline::Runnable::EG::LoadGFF3::ConstructFilenames;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Pipeline::Runnable::EG::Common::RunnableDB::Base');
+
+use File::Spec::Functions qw(catdir);
+
+sub write_output {
+  my ($self) = @_;
+  my $species                = $self->param_required('species');
+  my $source_dir             = $self->param_required('source_dir');
+  my $gff3_filename          = $self->param_required('gff3_filename');
+  my $gff3_tidy_filename     = $self->param_required('gff3_tidy_filename');
+  my $fasta_filename         = $self->param('fasta_filename');
+  my $genbank_filename       = $self->param('genbank_filename');
+  my $protein_fasta_filename = $self->param('protein_fasta_filename');
+  
+  my $output_ids = {
+    gff3_file => catdir($source_dir, $species, $gff3_filename),
+    gff3_tidy_file => catdir($source_dir, $species, $gff3_tidy_filename),
+  };
+  
+  if (defined $fasta_filename) {
+    $$output_ids{fasta_file} = catdir($source_dir, $species, $fasta_filename);
+  } else {
+    $$output_ids{fasta_file} = catdir($source_dir, $species, "genome.fa");
+  }
+  
+  if (defined $genbank_filename) {
+    $$output_ids{genbank_file} = catdir($source_dir, $species, $genbank_filename);
+  }
+  
+  if (defined $protein_fasta_filename) {
+    $$output_ids{protein_fasta_file} = catdir($source_dir, $species, $protein_fasta_filename);
+  }
+  
+  $self->dataflow_output_id($output_ids, 1);
+}
+
+1;
