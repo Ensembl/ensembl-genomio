7f07056c20783cf010326874a871c2001e168c9a
Matthieu Barba <mbarba@ebi.ac.uk>
2021-08-06 11:27:30 +0100

    Add stable_id generation for invalid ones (not by default)
    When a gene stable_id is not valid, it is possible to generate a temp
    one in the form TMP_ organism_abbrev _ number

--- lib/python/ensembl/brc4/runnable/process_gff3.py
+++ lib/python/ensembl/brc4/runnable/process_gff3.py
@@ -67,6 +67,7 @@ class process_gff3(eHive.BaseRunnable):
                 "exclude_seq_regions": [],
                 "validate_gene_id": True,
                 "min_id_length": 8,
+                "make_missing_stable_id": False,
                 }
 
     def run(self):
@@ -508,7 +509,10 @@ class process_gff3(eHive.BaseRunnable):
         funcann.append(feature_object)
     
     def normalize_gene_id(self, gene) -> str:
-        """Remove any unnecessary prefixes around the gene ID"""
+        """
+        Remove any unnecessary prefixes around the gene ID
+        Generate a new stable id if it is not recognized as valid
+        """
 
         prefixes = ("gene-", "gene:")
         new_gene_id = self.remove_prefixes(gene.id, prefixes)
@@ -525,11 +529,39 @@ class process_gff3(eHive.BaseRunnable):
                             new_gene_id = db + "_" + value
                             print("Using GeneID %s for stable_id instead of %s" % (new_gene_id, gene.id))
                             return new_gene_id
-                    raise Exception("Can't use invalid gene id for %s (no GeneID replacement found)" % gene)
+
+                # Make a new stable_id
+                if self.param("make_missing_stable_id"):
+                    new_id = self.generate_stable_id()
+                    print("New id: %s -> %s" % (new_gene_id, new_id))
+                    return new_id
                 else:
                     raise Exception("Can't use invalid gene id for %s" % gene)
         
         return new_gene_id
+    
+    def generate_stable_id(self):
+        """
+        Create a gene stable id
+        """
+        if self.param_exists("stable_id_prefix"):
+            prefix = self.param("stable_id_prefix")
+        else:
+            dat = self.param('genome_data')
+            org = dat["BRC4"]["organism_abbrev"]
+            prefix = "TMP_" + org + "_"
+            self.param("stable_id_prefix", prefix)
+        
+        if self.param_exists("current_stable_id_number"):
+            number = self.param("current_stable_id_number")
+        else:
+            number = 1
+        
+        number += 1
+        new_id = "%s%d" % (prefix, number)
+        self.param("current_stable_id_number", number)
+        
+        return new_id
     
     def valid_gene_id(self, name):
         """Check the gene id format"""
@@ -540,17 +572,22 @@ class process_gff3(eHive.BaseRunnable):
         
         # Trna (from tRNAscan)
         if re.search(r"^Trna", name):
-            print("Gene id is a Trna from tRNA-scan")
+            print("Gene id is a Trna from tRNA-scan: %s" % name)
             return False
         
         # Coordinates
-        #elif re.search(r"", name):
-        #    print("Gene id is a Trna from tRNA-scan")
-        #    return False
+        elif re.search(r'^.+:\d+..\d+', name):
+            print("Gene id is a coordinate: %s" % name)
+            return False
+
+        # Coordinates
+        elif re.search(r' ', name):
+            print("Gene id contains spaces: %s" % name)
+            return False
 
         # Min length
         elif len(name) <= min_length:
-            print("Gene id is too short (<%d)" % min_length)
+            print("Gene id is too short (<%d) %s" % (min_length, name))
             return False
         else:
             return True
