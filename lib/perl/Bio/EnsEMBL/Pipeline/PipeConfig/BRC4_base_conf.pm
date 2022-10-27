751eb6c102cb5511ac6b27a381e1b236742d6ee3
Matthieu Barba <mbarba@ebi.ac.uk>
2022-10-27 15:44:08 +0100

    Create a base for BRC pipeconfigs

---
+++ lib/perl/Bio/EnsEMBL/Pipeline/PipeConfig/BRC4_base_conf.pm
@@ -0,0 +1,88 @@
+=head1 LICENSE
+
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+Base config pipeline for BRC/Metazoa.
+
+=cut
+
+package Bio::EnsEMBL::Pipeline::PipeConfig::BRC4_base_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');
+
+sub make_resource {
+  my ($self, $conf) = @_;
+
+  my $res = {
+    'LSF' => _lsf_resource($conf),
+    'SLURM' => _slurm_resource($conf),
+  };
+  return $res;
+}
+
+sub _lsf_resource {
+  my ($conf) = @_;
+
+  my $mem = $conf->{memory};
+  my $cpus = $conf->{cpus};
+  my $queue = $conf->{queue};
+  my $time = $conf->{time};
+
+  if ($time and $time =~ /(\d\d):(\d\d):(\d\d)/) {
+    $time = "$1:$2";
+  }
+  
+  my @res_params = ("-M $mem", "-R 'rusage[mem=$mem]'");
+  push @res_params, "-q $queue" if $queue;
+  push @res_params, "-q $time" if $time;
+  push @res_params, $conf->{slurm_params} if $conf->{slurm_params};
+  my $res_string = join(" ", @res_params);
+  return $res_string;
+}
+
+sub _slurm_resource {
+  my ($conf) = @_;
+
+  my $mem = $conf->{memory};
+  my $cpus = $conf->{cpus};
+  my $queue = $conf->{queue};
+  my $time = $conf->{time};
+
+  # Prepare memory string
+  die("Memory needed for resource") if not $mem;
+  my $rmem;
+  if ($mem > 1000) {
+    $mem = int($mem/1000);
+    $rmem = $mem . 'g';
+  } else {
+    $rmem = $mem . "m";
+  }
+  
+  my @res_params = ("--mem=$rmem");
+  push @res_params, "--time=$time" if $time;
+  push @res_params, "--cpus=$cpus" if $cpus;
+  push @res_params, "--partition=$queue" if $queue;
+  push @res_params, $conf->{lsf_params} if $conf->{lsf_params};
+  my $res_string = join(" ", @res_params);
+  return $res_string;
+}
+
+1;
