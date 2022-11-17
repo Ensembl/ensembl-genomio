9a127552836c4fa4d33c8472b4da6ccce0a66c8f
Matthieu Barba <mbarba@ebi.ac.uk>
2022-11-17 12:29:10 +0000

    fix slurm custom params

--- lib/perl/Bio/EnsEMBL/Pipeline/PipeConfig/BRC4_base_conf.pm
+++ lib/perl/Bio/EnsEMBL/Pipeline/PipeConfig/BRC4_base_conf.pm
@@ -81,7 +81,7 @@ sub _slurm_resource {
   push @res_params, "--time=$time" if $time;
   push @res_params, "-c $cpus" if $cpus;
   push @res_params, "--partition=$queue" if $queue;
-  push @res_params, $conf->{lsf_params} if $conf->{lsf_params};
+  push @res_params, $conf->{slurm_params} if $conf->{slurm_params};
   my $res_string = join(" ", @res_params);
   return $res_string;
 }
