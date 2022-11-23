f82d479328cac151b48f02e2dfbeb3703a2755f2
Matthieu Barba <mbarba@ebi.ac.uk>
2022-11-23 14:57:43 +0000

    Create basic utils; add read/write json files

---
+++ lib/python/ensembl/brc4/runnable/utils.py
@@ -0,0 +1,44 @@
+#!env python3
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import Any
+from pathlib import Path
+import json
+
+
+class Utils:
+
+    @staticmethod
+    def print_json(path: Path, data: Any) -> None:
+        """Generic data json dumper to a file.
+        
+        Args:
+            path: Path to the json to create.
+            data: Any data to store.
+            """
+        with path.open("w") as json_out:
+            json_out.write(json.dumps(data, sort_keys=True, indent=4))
+ 
+    @staticmethod
+    def get_json(json_path: Path) -> Any:
+        """Generic data json loader.
+        
+        Args:
+            path: Path to the json file to load.
+            """
+        with json_path.open('r') as json_file:
+            return json.load(json_file)
