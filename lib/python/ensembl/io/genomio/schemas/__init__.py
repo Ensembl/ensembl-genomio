8bbedc4498b1e6331d5e499ba9acfc5bcccfd064
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2023-03-08 11:07:28 +0000

    add unit tests for 'schemas' module

--- lib/python/ensembl/io/genomio/schemas/__init__.py
+++ lib/python/ensembl/io/genomio/schemas/__init__.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Schemas module."""
 
 from .json_schema_factory import *
 from .json_schema_validator import *
