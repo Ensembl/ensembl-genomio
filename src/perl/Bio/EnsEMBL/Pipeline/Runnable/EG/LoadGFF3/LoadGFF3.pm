9f745d06c9e0138896f3609e9e2c37c4e8d737ab
vsitnik <39947971+vsitnik@users.noreply.github.com>
2023-05-15 20:10:25 +0100

    Update src/perl/Bio/EnsEMBL/Pipeline/Runnable/EG/LoadGFF3/LoadGFF3.pm
    Co-authored-by: Matthieu Barba <mbarba@ebi.ac.uk>

--- src/perl/Bio/EnsEMBL/Pipeline/Runnable/EG/LoadGFF3/LoadGFF3.pm
+++ src/perl/Bio/EnsEMBL/Pipeline/Runnable/EG/LoadGFF3/LoadGFF3.pm
@@ -458,7 +458,7 @@ sub add_exons {
   my $different_strands = scalar(keys %{{ map { $_=>1 } @strands }});
   my $force_ranking = ($different_strands > 1 || scalar(@outlayers) > 0) ? 1 : 0;
 
-  # get major trandness to deal with transspliced
+  # Use the strandness majority to decide in which order the exons of transspliced genes are ranked
   my $strandness = sum0 @strands;
   my $sorting = ($strandness > 0)? \&sort_genomic : \&sort_genomic_desc;
 
