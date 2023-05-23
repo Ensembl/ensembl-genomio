aed1ba5bdfb06c37152a8d18c8dd7abfe4d7a6b3
Matthieu Barba <mbarba@ebi.ac.uk>
2023-05-23 11:06:18 +0100

    Fix some tests

--- src/python/ensembl/io/genomio/schemas/__init__.py
+++ src/python/ensembl/io/genomio/schemas/__init__.py
@@ -12,4 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .json_schema_validator import *
+from .json_schema_factory import *
+
 """Schemas module."""
