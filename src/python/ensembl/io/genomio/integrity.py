8cc72b7d4fd015218a6fde3a7f936bd9c46ed149
Matthieu Barba <mbarba@ebi.ac.uk>
2023-06-07 12:36:33 +0100

    Get lengths of gene from gff separately

--- src/python/ensembl/io/genomio/integrity.py
+++ src/python/ensembl/io/genomio/integrity.py
@@ -29,10 +29,13 @@ from typing import Any, Dict, List
 
 import argschema
 from BCBio import GFF
-from Bio import SeqIO
+from Bio import SeqIO, SeqFeature
 
 from ensembl.io.genomio.utils.archive_utils import open_gz_file
 from ensembl.io.genomio.utils.json_utils import get_json
+
+
+Lengths = Dict[str, int]
 
 
 class InvalidIntegrityError(Exception):
@@ -245,11 +248,11 @@ class Manifest:
             are stored with their corresponding lengths.
         """
 
-        seqs = {}
-        genes = {}
-        peps = {}
-        all_peps = {}
-        tes = {}
+        seqs: Lengths = {}
+        genes: Lengths = {}
+        peps: Lengths = {}
+        all_peps: Lengths = {}
+        tes: Lengths = {}
 
         with open_gz_file(gff3_path) as gff3_handle:
             gff = GFF.parse(gff3_handle)
@@ -260,28 +263,7 @@ class Manifest:
                     feat_length = abs(feat.location.end - feat.location.start)
                     # Store gene id and length
                     if feat.type in ["gene", "ncRNA_gene", "pseudogene"]:
-                        gene_id = feat.id
-                        if not self.brc_mode:
-                            gene_id = gene_id.replace("gene:", "")
-                        genes[gene_id] = abs(feat.location.end - feat.location.start)
-                        # Get CDS id and length
-                        for feat2 in feat.sub_features:
-                            if feat2.type in ("mRNA", "pseudogenic_transcript"):
-                                length = {}
-                                for feat3 in feat2.sub_features:
-                                    if feat3.type == "CDS":
-                                        pep_id = feat3.id
-                                        if not self.brc_mode:
-                                            pep_id = pep_id.replace("CDS:", "")
-                                        if pep_id not in length:
-                                            length[pep_id] = 0
-                                        length[pep_id] += abs(feat3.location.end - feat3.location.start)
-                                for pep_id, pep_length in length.items():
-                                    # Store length for translations, add pseudo translations separately
-                                    pep_length = floor(pep_length / 3) - 1
-                                    if feat.type != "pseudogene":
-                                        peps[pep_id] = pep_length
-                                    all_peps[pep_id] = pep_length
+                        self._retrieve_gff_gene_lengths(feat, genes, peps, all_peps)
                     if feat.type == "transposable_element":
                         tes[feat.id] = feat_length
 
@@ -293,6 +275,32 @@ class Manifest:
             "transposable_elements": tes,
         }
         return stats
+
+    def _retrieve_gff_gene_lengths(
+        self, feat: SeqFeature, genes: Lengths, peps: Lengths, all_peps: Lengths
+    ) -> None:
+        gene_id = feat.id
+        if not self.brc_mode:
+            gene_id = gene_id.replace("gene:", "")
+        genes[gene_id] = abs(feat.location.end - feat.location.start)
+        # Get CDS id and length
+        for feat2 in feat.sub_features:
+            if feat2.type in ("mRNA", "pseudogenic_transcript"):
+                length = {}
+                for feat3 in feat2.sub_features:
+                    if feat3.type == "CDS":
+                        pep_id = feat3.id
+                        if not self.brc_mode:
+                            pep_id = pep_id.replace("CDS:", "")
+                        if pep_id not in length:
+                            length[pep_id] = 0
+                        length[pep_id] += abs(feat3.location.end - feat3.location.start)
+                for pep_id, pep_length in length.items():
+                    # Store length for translations, add pseudo translations separately
+                    pep_length = floor(pep_length / 3) - 1
+                    if feat.type != "pseudogene":
+                        peps[pep_id] = pep_length
+                    all_peps[pep_id] = pep_length
 
     def get_agp_seq_regions(self, agp_dict):
         """AGP files describe the assembly of larger sequence objects using smaller objects.
