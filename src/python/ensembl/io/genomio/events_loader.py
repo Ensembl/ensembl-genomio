fc953bda8f1dbf1d95eb3aaeb96624016a34a57a
Matthieu Barba <mbarba@ebi.ac.uk>
2023-07-27 11:35:13 +0100

    Extend collection to load events/write events

--- src/python/ensembl/io/genomio/events_loader.py
+++ src/python/ensembl/io/genomio/events_loader.py
@@ -22,7 +22,8 @@ cf the load_events functions for the events tab file format.
 from dataclasses import dataclass
 from os import PathLike
 from pathlib import Path
-from typing import Dict, List
+import re
+from typing import Dict, Generator, List, Tuple
 
 import argschema
 from sqlalchemy.engine import URL
@@ -41,6 +42,10 @@ class IdEvent:
     release: str
     release_date: str
 
+    def __str__(self) -> str:
+        fields = [self.from_id, self.to_id, self.release, self.release_date]
+        return "\t".join(fields)
+
 
 class MapSession:
     """Simple mapping_sessions representation from the input file"""
@@ -55,58 +60,119 @@ class MapSession:
         self.events.append(event)
 
 
-def load_events(input_file: PathLike) -> List[IdEvent]:
-    """Load events from input file.
-    Expected tab file columns: old_id, new_id, event_name, release, release_date
+class EventCollection:
+    """ "Collection of events with loader/writer in various formats."""
 
-    """
-    events: List[IdEvent] = []
+    def __init__(self) -> None:
+        self.events: List[IdEvent] = []
 
-    with Path(input_file).open("r") as events_fh:
-        for line in events_fh:
-            line.strip()
-            if line == "":
-                continue
-            (from_id, to_id, _, release, release_date) = line.split("\t")
-            event = IdEvent(from_id=from_id, to_id=to_id, release=release, release_date=release_date)
-            events.append(event)
-    return events
+    def load_events(self, input_file: PathLike):
+        """Load events from input file.
+        Expected tab file columns: old_id, new_id, event_name, release, release_date
 
+        """
+        events: List[IdEvent] = []
 
-def write_events(session: Session, events: List[IdEvent], update: bool = False) -> None:
-    """Insert the events in the core database.
-    A mapping session is created for each different 'release'.
+        with Path(input_file).open("r") as events_fh:
+            for line in events_fh:
+                line.strip()
+                if line == "":
+                    continue
+                (from_id, to_id, _, release, release_date) = line.split("\t")
+                event = IdEvent(from_id=from_id, to_id=to_id, release=release, release_date=release_date)
+                events.append(event)
+        self.events = events
 
-    """
-    # First, create mapping_sessions based on the release
-    mappings: Dict[str, MapSession] = {}
-    for event in events:
-        release = event.release
+    def load_events_from_gene_diff(self, input_file: PathLike):
+        """Load events from input file from gene_diff."""
+        events: List[IdEvent] = []
+        release_name = "release_name"
+        release_date = "release_date"
 
-        if release in mappings:
-            mappings[release].add_event(event)
-        else:
-            mappings[release] = MapSession(release, event.release_date)
+        with Path(input_file).open("r") as events_fh:
+            for line in events_fh:
+                if line.startswith("//") or line == "":
+                    continue
+                (_, event_string, _) = line.split("\t")
+                for pair in self._parse_gene_diff_event(event_string):
+                    (from_id, to_id) = pair
+                    event = IdEvent(
+                        from_id=from_id, to_id=to_id, release=release_name, release_date=release_date
+                    )
+                    events.append(event)
+        self.events = events
 
-    # Then, add the mapping, and the events for this mapping
-    for release, mapping in mappings.items():
-        print(f"Adding mapping for release {release} ({len(mapping.events)} events)")
-        if update:
-            map_session = MappingSession(new_release=mapping.release, created=mapping.release_date)
-            session.add(map_session)
-            session.flush()
-            session.refresh(map_session)
-            for event in mapping.events:
-                id_event = StableIdEvent(
-                    mapping_session_id=map_session.mapping_session_id,
-                    old_stable_id=event.from_id,
-                    new_stable_id=event.to_id,
-                    id_type="gene",
-                    old_version=1,
-                    new_version=1,
-                )
-                session.add(id_event)
-            session.commit()
+    def _parse_gene_diff_event(self, event_string: str) -> Generator[Tuple[str, str], None, None]:
+        """Gets all the pairs of IDs from an event string from gene diff."""
+        splitter = re.compile(r"(~|\+|=\+|=-|=!|=|>|<)")
+        parts = re.split(splitter, event_string)
+        if len(parts) != 3:
+            return
+        [from_ids, sep, to_ids] = parts
+
+        # Identical gene: no need to keep in the history
+        if sep == "~":
+            return
+
+        for from_id in from_ids.split(":"):
+            for to_id in to_ids.split(":"):
+                yield (from_id, to_id)
+
+    def remap_to_ids(self, map_dict: Dict[str, str]):
+        """Using a mapping dict, remap the to_id of all events."""
+
+        no_map = 0
+        for event in self.events:
+            if event.to_id in map_dict:
+                event.to_id = map_dict[event.to_id]
+            else:
+                print(f"No map for to_id {event.to_id}")
+                no_map += 1
+
+        if no_map:
+            raise ValueError(f"No map for {no_map} event to_ids")
+
+    def write_events_to_file(self, output_file: PathLike) -> None:
+        """Write the events to a file."""
+        with Path(output_file).open("w") as out_fh:
+            print(f"Write {len(self.events)} events to {output_file}")
+            for event in self.events:
+                out_fh.write(f"{event}\n")
+
+    def write_events_to_db(self, session: Session, update: bool = False) -> None:
+        """Insert the events in the core database.
+        A mapping session is created for each different 'release'.
+
+        """
+        # First, create mapping_sessions based on the release
+        mappings: Dict[str, MapSession] = {}
+        for event in self.events:
+            release = event.release
+
+            if release in mappings:
+                mappings[release].add_event(event)
+            else:
+                mappings[release] = MapSession(release, event.release_date)
+
+        # Then, add the mapping, and the events for this mapping
+        for release, mapping in mappings.items():
+            print(f"Adding mapping for release {release} ({len(mapping.events)} events)")
+            if update:
+                map_session = MappingSession(new_release=mapping.release, created=mapping.release_date)
+                session.add(map_session)
+                session.flush()
+                session.refresh(map_session)
+                for event in mapping.events:
+                    id_event = StableIdEvent(
+                        mapping_session_id=map_session.mapping_session_id,
+                        old_stable_id=event.from_id,
+                        new_stable_id=event.to_id,
+                        id_type="gene",
+                        old_version=1,
+                        new_version=1,
+                    )
+                    session.add(id_event)
+                session.commit()
 
 
 class InputSchema(argschema.ArgSchema):
@@ -149,10 +215,11 @@ def main() -> None:
     )
     dbc = DBConnection(db_url)
 
-    events = load_events(mod.args.get("input_file"))
+    collection = EventCollection()
+    collection.load_events(mod.args.get("input_file"))
 
     with dbc.session_scope() as session:
-        write_events(session, events, mod.args["update"])
+        collection.write_events_to_db(session, mod.args["update"])
 
 
 if __name__ == "__main__":
