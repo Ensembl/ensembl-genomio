7262e6a5d21d250b8eb5568c4da2ac7d9de0ff02
Dishalodha <87130059+Dishalodha@users.noreply.github.com>
2023-08-15 16:18:14 +0100

    Update LoadGFF3.pm

--- src/perl/Bio/EnsEMBL/Pipeline/Runnable/EG/LoadGFF3/LoadGFF3.pm
+++ src/perl/Bio/EnsEMBL/Pipeline/Runnable/EG/LoadGFF3/LoadGFF3.pm
@@ -93,8 +93,7 @@ sub param_defaults {
     exon_types      => ['exon', 'pseudogenic_exon'],
     cds_types       => ['CDS'],
     utr_types       => ['five_prime_UTR', 'three_prime_UTR'],
-    ignore_types    => [
-                        'match', 'match_part',
+    ignore_types    => ['match', 'match_part',
                         'sequence_feature',
                         'cDNA_match', 'nucleotide_match', 'protein_match',
                         'polypeptide', 'protein',
