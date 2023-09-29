acfd7709ef10340ae592e785666f5f72832adfa9
Matthieu Barba <mbarba@ebi.ac.uk>
2023-09-29 15:36:11 +0100

    Add basic assembly comparison

--- src/python/ensembl/io/genomio/genome_metadata/compare_genome_stats.py
+++ src/python/ensembl/io/genomio/genome_metadata/compare_genome_stats.py
@@ -24,8 +24,50 @@ from typing import Any, Dict
 import argschema
 
 
-def compare_assembly(ncbi: Dict, core: Dict) -> Dict:
-    comp = {}
+def compare_assembly(ncbi_main: Dict, ncbi_organella: Dict, core: Dict) -> Dict:
+
+    # First count the organella
+    ncbi_num_organella = len(ncbi_organella)
+    core_num_organella = 0
+    core_num_chrs = 0
+    for loc, loc_count in core["locations"].items():
+        if loc == "nuclear_chromosome":
+            core_num_chrs += loc_count
+        else:
+            core_num_organella += loc_count
+    
+    print(f"Core Chromosomes: {core_num_chrs}")
+    print(f"Core Organella: {core_num_organella}")
+
+    # Our core stats count Organella chromosomes, sanity check here
+    core_adjusted_chrs = core["coord_system"].get("chromosome", 0) - core_num_organella
+    if core_adjusted_chrs != core_num_chrs:
+        raise ValueError(f"Core stats chromosomes number doesn't add up: {core_adjusted_chrs} vs {core_num_chrs}")
+    
+    core_num_scaffolds = core["coord_system"].get("scaffold", 0)
+    print(f"Core scaffolds: {core_num_scaffolds}")
+
+    # NCBI includes the chromodomes in its stats
+    core_adjusted_scaffolds = core_num_scaffolds + core_num_chrs
+    print(f"Core adjusted scaffolds: {core_adjusted_scaffolds}")
+
+    # Compare:
+    # The number of scaffolds
+    comp = {
+        "scaffolds": _diff_count(ncbi_main["number_of_scaffolds"], core_adjusted_scaffolds),
+        "chromosomes": _diff_count(ncbi_main["total_number_of_chromosomes"], core_adjusted_chrs),
+        "organella": _diff_count(ncbi_num_organella, core_num_organella),
+    }
+
+    return comp
+
+
+def _diff_count(ncbi_count: int, core_count: int) -> Dict:
+    comp = {
+        "ncbi": ncbi_count,
+        "core": core_count,
+        "diff": ncbi_count - core_count
+    }
     return comp
 
 
@@ -36,13 +78,22 @@ def compare_annotation(ncbi: Dict, core: Dict) -> Dict:
 
 def compare_stats(ncbi: Dict, core: Dict) -> Dict:
     """Compare stats from NCBI and our core."""
+
+    ncbi_assembly_stats = ncbi.get("assembly_stats")
+    ncbi_organella = ncbi.get("organelle_info")
+    ncbi_annotation_stats = ncbi.get("annotation_stats")
+    core_assembly_stats = core.get("assembly_stats")
+    core_annotation_stats = core.get("annotation_stats")
+
     comp = {
-        "assembly_diff": compare_assembly(ncbi, core),
-        "annotation_diff": compare_annotation(ncbi, core)
+        "assembly_diff": compare_assembly(ncbi_assembly_stats, ncbi_organella, core_assembly_stats),
     }
+    if core_annotation_stats is not None and ncbi_annotation_stats is not None:
+        comp["annotation_diff"] = compare_annotation(ncbi_annotation_stats, core_annotation_stats)
 
     new_core = {
         "core_stats": core,
+        "ncbi_stats": {"assembly_stats": ncbi_assembly_stats, "anotation_stats": core_annotation_stats},
         "ncbi_comparison": comp
     }
     return new_core
@@ -62,7 +113,7 @@ def main() -> None:
     args = mod.args
 
     with open(args["ncbi_stats"]) as ncbi_fh:
-        ncbi_stats = json.load(ncbi_fh)
+        ncbi_stats = json.load(ncbi_fh)["reports"][0]
     with open(args["core_stats"]) as core_fh:
         core_stats = json.load(core_fh)
     all_stats = compare_stats(ncbi_stats, core_stats)
