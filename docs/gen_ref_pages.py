4c6546c0ac99f1cea96dc4ce7f534b9b503b991d
Lahcen Campbell <32962169+ens-LCampbell@users.noreply.github.com>
2023-11-14 15:21:37 +0000

    Update docs/gen_ref_pages.py
    remove unused comments
    
    Co-authored-by: J. Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -22,9 +22,9 @@ for path in sorted(Path("src").rglob("*.py")):
 
     nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:  # 
-        identifier = ".".join(parts)  # 
-        print("::: " + identifier, file=fd)  # 
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
 
     mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)  # 
 
