004b46d10da4f712956ed210485c0d841897d9fb
Matthieu Barba <mbarba@ebi.ac.uk>
2023-11-29 11:58:01 +0000

    Add tests for CoreServer.get_cores

--- src/python/tests/database/test_core_server.py
+++ src/python/tests/database/test_core_server.py
@@ -16,6 +16,8 @@
 
 """
 
+from typing import Dict, Union
+import pytest
 from unittest.mock import MagicMock
 
 from ensembl.io.genomio.database import CoreServer
@@ -27,21 +29,41 @@ TEST_LIST = [
     "prefix_speciesC_genus_core_60_110_1",
     "prefix_speciesD_genus_sp_core_60_110_1",
     "speciesE_genus_core_61_110_1",
-    "speciesF_genus_sp_core_61_110_1",
+    "speciesF_genus_core_61_111_1",
 ]
 
 
 class TestCoreServer:
     """Tests for the database.core_server module."""
 
-    def test_get_cores(self):
+    @pytest.mark.parametrize(
+        "parameters, output_size",
+        [
+            ({}, len(TEST_LIST)),
+            ({"build": 60}, 4),
+            ({"build": 61}, 2),
+            ({"prefix": "foobar"}, 0),
+            ({"prefix": "prefix_"}, 2),
+            ({"version": 110}, 5),
+            ({"version": 111}, 1),
+            ({"version": 999}, 0),
+            ({"dbname_re": r""}, len(TEST_LIST)),
+            ({"dbname_re": r"foobar"}, 0),
+            ({"dbname_re": r"^species._.*"}, 4),
+            ({"dbname_re": r"species._.*_sp_"}, 2),
+            ({"dbname_re": r"^prefix_"}, 2),
+            ({"prefix": "prefix_", "build": 60}, 2),
+            ({"build": 61, "version": 110}, 1),
+        ],
+    )
+    def test_get_cores(self, parameters: Dict[str, Union[str, int]], output_size: int):
         """Test the CoreServer.get_cores() method."""
+
+        # Fake server with mock get_all_core_names()
         server_url = "sqlite:///:memory:"
         server = CoreServer(server_url)
         server.get_all_core_names = MagicMock(return_value=TEST_LIST)
 
-        all_cores = server.get_cores()
-        assert len(all_cores) == len(TEST_LIST)
-
-        prefixed = server.get_cores(prefix="prefix_")
-        assert len(prefixed) == 2
+        # NB: get_cores() gets its data from the mocked get_all_core_names()
+        all_cores = server.get_cores(**parameters)
+        assert len(all_cores) == output_size
