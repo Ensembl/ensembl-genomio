576d44e34a8c48cfd7c937f06891a9fe11b07121
dishalodha <dishalodha@ebi.ac.uk>
2024-01-16 09:50:46 +0000

    add a test file for genbank download

---
+++ src/python/tests/test_download.py
@@ -0,0 +1,73 @@
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit testing of :mod:`ensembl.io.genomio.genbank.download` module.
+
+The unit testing is divided into one test class per submodule/class found in this module, and one test method
+per public function/class method.
+
+Typical usage example::
+    $ pytest test_download.py
+
+"""
+
+
+import pytest
+from pathlib import Path
+from unittest.mock import patch, MagicMock
+
+from ensembl.io.genomio.genbank.download import download_genbank, DownloadError
+
+@pytest.mark.parametrize(
+        "accession",
+        [
+            ("CM023531.1"),
+        ], 
+    )
+@patch('ensembl.io.genomio.genbank.download.requests.get')
+class TestDownload:
+    
+    def test_successful_download(self, mock_requests_get, tmp_dir: Path, accession):
+        """Tests the 'download_genbank()' method.
+
+        Args:
+            tmp_dir: Session-scoped temporary directory fixture.
+            accession: Genbank accession to be downloaded.
+        """
+        content = b"The genbank download for the following accession"
+        mock_requests_get.return_value.status_code = 200
+        mock_requests_get.return_value.content = content
+        output_file = tmp_dir / f"{accession}.gb"
+        download_genbank(accession, output_file)
+
+        mock_requests_get.assert_called_once_with(
+            "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi",
+            params={"db": "nuccore", "rettype": "gbwithparts", "retmode": "text", "id": accession},
+            timeout=60
+        )
+        
+        all_calls = mock_requests_get.call_args_list
+        print(all_calls)
+        # Assert that the content was written to the temporary file
+        with open(output_file, 'rb') as f:
+            content = f.read()
+        assert content == content
+
+    def test_failed_download(self, mock_requests_failed, tmp_dir: Path, accession):
+        output_file = tmp_dir / f"{accession}.gb"
+        mock_requests_failed.return_value.status_code = 400
+
+        with pytest.raises(DownloadError, match='Could not download the genbank') as error:
+            download_genbank(accession, output_file)
+        
