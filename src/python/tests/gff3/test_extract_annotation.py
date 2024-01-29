a97b2d6651a985abf7209945ff3f5a5c97722110
Matthieu Barba <mbarba@ebi.ac.uk>
2024-01-29 16:05:21 +0000

    Update test after fixes

--- src/python/tests/gff3/test_extract_annotation.py
+++ src/python/tests/gff3/test_extract_annotation.py
@@ -39,38 +39,39 @@ from ensembl.io.genomio.gff3.extract_annotation import (
 @pytest.mark.parametrize(
     "description, feature_id, output",
     [
-        ("", None, False),
+        ("", [], False),
         ("", "PROTID12345", False),
-        ("PROTID12345", "PROTID12345", False),
-        ("ProtId12345", "PROTID12345", False),
-        ("hypothetical PROTID12345 (ProtId12345)", "PROTID12345", False),
-        ("hypothetical protein", None, False),
-        ("hypothetical_protein", None, False),
-        ("Hypothetical protein", None, False),
-        ("hypothetical protein PROTID12345", "PROTID12345", False),
-        ("hypothetical protein ProtId12345", "PROTID12345", False),
-        ("hypothetical protein (ProtId12345)", "PROTID12345", False),
-        ("hypothetical protein (fragment)", None, False),
-        ("hypothetical protein, variant", None, False),
-        ("hypothetical protein, variant 2", None, False),
-        ("hypothetical protein - conserved", None, False),
-        ("hypothetical protein, conserved", None, False),
-        ("Hypothetical_protein_conserved", None, False),
-        ("Hypothetical conserved protein", None, False),
-        ("conserved hypothetical protein", None, False),
-        ("conserved hypothetical protein, putative", None, False),
-        ("conserved protein, unknown function", None, False),
-        ("putative protein", None, False),
-        ("putative_protein", None, False),
-        ("hypothetical RNA", None, False),
-        ("unspecified product", None, False),
-        ("Unspecified product", None, False),
-        ("conserved hypothetical transmembrane protein", None, True),
-        ("unknown gene", None, False),
-        ("unknown function", None, False),
+        ("PROTID12345", ["PROTID12345"], False),
+        ("ProtId12345", ["PROTID12345"], False),
+        ("hypothetical PROTID12345 (ProtId12345)", ["PROTID12345"], False),
+        ("hypothetical protein", [], False),
+        ("hypothetical_protein", [], False),
+        ("Hypothetical protein", [], False),
+        ("hypothetical protein PROTID12345", ["PROTID12345"], False),
+        ("hypothetical protein ProtId12345", ["PROTID12345"], False),
+        ("hypothetical protein (ProtId12345)", ["PROTID12345"], False),
+        ("hypothetical protein (fragment)", [], False),
+        ("hypothetical protein, variant", [], False),
+        ("hypothetical protein, variant 2", [], False),
+        ("hypothetical protein - conserved", [], False),
+        ("hypothetical protein, conserved", [], False),
+        ("Hypothetical_protein_conserved", [], False),
+        ("Hypothetical conserved protein", [], False),
+        ("conserved hypothetical protein", [], False),
+        ("conserved hypothetical protein, putative", [], False),
+        ("conserved protein, unknown function", [], False),
+        ("putative protein", [], False),
+        ("putative_protein", [], False),
+        ("hypothetical RNA", [], False),
+        ("unspecified product", [], False),
+        ("Unspecified product", [], False),
+        ("conserved hypothetical transmembrane protein", [], True),
+        ("unknown gene", [], False),
+        ("unknown function", [], False),
+        ("uncharacterized PROTID12345", ["PROTID12345"], False),
     ],
 )
-def test_product_is_informative(description: str, feature_id: Optional[str], output: bool) -> None:
+def test_product_is_informative(description: str, feature_id: Optional[List[str]], output: bool) -> None:
     """Tests the `FunctionalAnnotations.product_is_informative()` method."""
     assert FunctionalAnnotations.product_is_informative(description, feature_id) == output
 
@@ -116,8 +117,7 @@ def test_add_feature_name(feat_id: str, feat_name: str, expected_synonyms: List[
 
     seq_feat_type = "gene"
     feat_type = "gene"
-    feature = SeqFeature(type=seq_feat_type, id=feat_id)
-    feature.qualifiers["Name"] = [feat_name]
+    feature = SeqFeature(type=seq_feat_type, id=feat_id, qualifiers={"Name": [feat_name]})
     annot.add_feature(feature, feat_type)
     loaded_feat = annot.features[feat_type][feature.id]
     try:
@@ -297,10 +297,10 @@ def test_transfer_descriptions(
     transcript_name = "tran_A"
     one_gene = SeqFeature(type="gene", id=gene_name)
     if gene_desc:
-        one_gene.qualifiers["Name"] = [gene_desc]
+        one_gene.qualifiers["description"] = [gene_desc]
     one_transcript = SeqFeature(type="mRNA", id=transcript_name)
     if transc_desc:
-        one_transcript.qualifiers = {"Name": [transc_desc]}
+        one_transcript.qualifiers = {"product": [transc_desc]}
     one_translation = SeqFeature(type="CDS", id="cds_A")
     if transl_desc:
         one_translation.qualifiers = {"product": [transl_desc]}
