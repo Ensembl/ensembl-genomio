38b919b97f461d36cb44c375347c363641649ca7
Matthieu Barba <mbarba@ebi.ac.uk>
2024-01-30 16:54:04 +0000

    Implement update

--- src/python/ensembl/io/genomio/annotation/load.py
+++ src/python/ensembl/io/genomio/annotation/load.py
@@ -41,31 +41,32 @@ def get_core_data(session: Session, table: str) -> Dict[str, str]:
 
     if table == "gene":
         stmt = (
-            select(Gene.stable_id, Gene.description, Xref.dbprimary_acc)
-            .select_from(Gene).join(ObjectXref, Gene.gene_id == ObjectXref.ensembl_id)
+            select(Gene.gene_id, Gene.stable_id, Gene.description, Xref.dbprimary_acc)
+            .select_from(Gene)
+            .join(ObjectXref, Gene.gene_id == ObjectXref.ensembl_id)
             .join(Xref)
             .where(ObjectXref.ensembl_object_type == "gene")
         )
     elif table == "transcript":
         stmt = (
-            select(Transcript.stable_id, Transcript.description, Xref.dbprimary_acc)
-            .select_from(Transcript).join(ObjectXref, Transcript.transcript_id == ObjectXref.ensembl_id)
+            select(Transcript.transcript_id, Transcript.stable_id, Transcript.description, Xref.dbprimary_acc)
+            .select_from(Transcript)
+            .join(ObjectXref, Transcript.transcript_id == ObjectXref.ensembl_id)
             .join(Xref)
             .where(ObjectXref.ensembl_object_type == "transcript")
         )
 
     feat_data = {}
     for row in session.execute(stmt):
-        (name, desc, xref_name) = row
-        feat_struct = (name, desc)
+        (feat_id, name, desc, xref_name) = row
+        feat_struct = (feat_id, name, desc)
         feat_data[name] = feat_struct
         feat_data[xref_name] = feat_struct
     return feat_data
 
 
 def load_descriptions(session: Session, func_file: Path, report: bool = False, update: bool = False) -> None:
-    """Load gene and transcript descriptions in a core database.
-    """
+    """Load gene and transcript descriptions in a core database."""
     func = get_json(func_file)
     logging.info(f"{len(func)} annotations from {func_file}")
     for table in ("gene", "transcript"):
@@ -83,6 +84,7 @@ def load_descriptions(session: Session, func_file: Path, report: bool = False, u
             "no_new": 0,
         }
         # Compare, only keep the descriptions that have changed
+        to_update = []
         for new_feat in feat_func:
             if "description" not in new_feat:
                 stats["no_new"] += 1
@@ -92,28 +94,38 @@ def load_descriptions(session: Session, func_file: Path, report: bool = False, u
             except KeyError:
                 stats["not_found"] += 1
                 continue
-            
+
             new_id = new_feat["id"]
-            cur_id = current_feat[0]
             new_desc = new_feat["description"]
-            cur_desc = current_feat[1]
+            (row_id, cur_id, cur_desc) = current_feat
             if not cur_desc:
                 cur_desc = ""
 
             if new_desc == cur_desc:
                 stats["same"] += 1
                 continue
-            
+
             stats["differs"] += 1
             if report:
                 line = (table, new_id, cur_id, cur_desc, new_desc)
                 print("\t".join(line))
+            if update:
+                update_key = f"{table}_id"
+                to_update.append({update_key: row_id, "description": new_desc})
 
         # Show stats for this feature type
         for stat, count in stats.items():
             if count == 0:
                 continue
             logging.info(f"{stat} = {count}")
+
+        if update:
+            logging.info(f"Now updating {len(to_update)} rows...")
+            if table == "gene":
+                table_object = Gene
+            elif table == "transcript":
+                table_object = Transcript
+            session.execute(update(table_object), to_update)
 
 
 def main() -> None:
@@ -130,6 +142,7 @@ def main() -> None:
     dbc = DBConnection(args.url)
     with dbc.session_scope() as session:
         load_descriptions(session, args.func_file, args.report, args.update)
+
 
 if __name__ == "__main__":
     main()
