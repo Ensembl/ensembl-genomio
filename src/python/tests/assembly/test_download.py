9347fbd1eb781348e997526295e7ab7ca5e90213
Lahcen Campbell <lcampbell@ebi.ac.uk>
2024-01-31 11:11:36 +0000

    Add test test_md5_files

--- src/python/tests/assembly/test_download.py
+++ src/python/tests/assembly/test_download.py
@@ -34,69 +34,95 @@ from ftplib import FTP
 
 
 from ensembl.io.genomio.assembly.download import FTPConnection, FTPConnectionError
-from ensembl.io.genomio.assembly.download import get_checksums
+from ensembl.io.genomio.assembly.download import get_checksums, md5_files
 
 
-class TestDownloadAssembly:
-    """Tests class for `download_assembly` module."""
+# class TestDownloadAssembly:
+#     """Tests class for `download_assembly` module."""
 
-    @pytest.mark.parametrize(
-        "ftp_url, sub_dir, expectation",
-        [
-            ("ftp.ncbi.nlm.nih.gov", "genomes/all/GCA/017/607/445", does_not_raise()),  # normal case
-            (
-                "ftp.ncbi.nlm.nih.gov",
-                "genomes/all/GCA/017/607/445",
-                pytest.raises(FTPConnectionError),
-            ),  # abnormal case, should run, but fail assert
-            ("ftp.ncbi.nlm.nih.gov", None, pytest.raises(FTPConnectionError)),  # bad case no subdir path
-            (
-                "ftp.remote.fake.gov",
-                "genomes/all/GCA/017/607/445",
-                pytest.raises(FTPConnectionError),
-            ),  # bad case incorrect ftp_url
-            (
-                "ftp.ncbi.nlm.nih.gov",
-                "genomes/GCA/017/607/445",
-                pytest.raises(FTPConnectionError),
-            ),  # bad case malformed subdir path
-        ],
-    )
-    @patch("ensembl.io.genomio.assembly.download.FTP", autospec=True)
-    def test_ftp_connection(
-        self,
-        mock_ftp_constructor: Mock,
-        ftp_url: str,
-        sub_dir: str,
-        expectation: ContextManager,
-    ):
-        """Tests the FTPConnection class method 'establish_ftp()'
-
-        Args:
-            mock_ftp_constructor:
-            ftp_url:
-            sub_dir:
-        """
-
-        mock_ftp = mock_ftp_constructor.return_value
-        mock_ftp.pwd = MagicMock(return_value="ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/607/445/")
-        connected_ftp = FTPConnection.establish_ftp(mock_ftp, ftp_url, sub_dir)
-        listing = connected_ftp.pwd()
-        assert listing == "ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/607/445/"
-        connected_ftp.pwd.assert_called_once()
 
 @pytest.mark.parametrize(
-    'checksum_file, expectation',
+    "ftp_url, sub_dir, expectation",
     [
-        ("normal_md5_checksums.txt", does_not_raise()), #Normal case
-        ("malformed_md5_checksums.txt", pytest.raises(ValueError)), #Bad md5sum file (tab separated)
-        (None, pytest.raises(TypeError)), #No md5 checksum File
-    ]
+        ("ftp.ncbi.nlm.nih.gov", "genomes/all/GCA/017/607/445", does_not_raise()),  # normal case
+        (
+            "ftp.ncbi.nlm.nih.gov",
+            "genomes/all/GCA/017/607/445",
+            pytest.raises(FTPConnectionError),
+        ),  # abnormal case, should run, but fail assert
+        ("ftp.ncbi.nlm.nih.gov", None, pytest.raises(FTPConnectionError)),  # bad case no subdir path
+        (
+            "ftp.remote.fake.gov",
+            "genomes/all/GCA/017/607/445",
+            pytest.raises(FTPConnectionError),
+        ),  # bad case incorrect ftp_url
+        (
+            "ftp.ncbi.nlm.nih.gov",
+            "genomes/GCA/017/607/445",
+            pytest.raises(FTPConnectionError),
+        ),  # bad case malformed subdir path
+    ],
 )
-def test_checksums(data_dir: Path, checksum_file: Path, expectation: ContextManager) -> None:
+@patch("ensembl.io.genomio.assembly.download.FTP", autospec=True)
+def test_ftp_connection(
+    mock_ftp_constructor: Mock,
+    ftp_url: str,
+    sub_dir: str,
+    expectation: ContextManager,
+):
+    """Tests the FTPConnection class method 'establish_ftp()'
+    Args:
+        mock_ftp_constructor:
+        ftp_url:
+        sub_dir:
+    """
+    mock_ftp = mock_ftp_constructor.return_value
+    mock_ftp.pwd = MagicMock(return_value="ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/607/445/")
+    connected_ftp = FTPConnection.establish_ftp(mock_ftp, ftp_url, sub_dir)
+    listing = connected_ftp.pwd()
+    assert listing == "ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/607/445/"
+    connected_ftp.pwd.assert_called_once()
+
+#################
+@pytest.mark.dependency(name="test_checksums")
+@pytest.mark.parametrize(
+    "checksum_file, checksum, expectation",
+    [
+        (Path("md5checksums.txt"), "40df91d5c40cb55621c4c92201da6834", does_not_raise()),  # Normal case
+        (
+            Path("malformed_md5_checksums.txt"),
+            None,
+            pytest.raises(ValueError),
+        ),  # Bad md5sum file (tab separated, expects two \w)
+        (Path("dir_name"), None, does_not_raise()),  # pass a dir instead of checksum
+    ],
+)
+def test_checksums(data_dir: Path, checksum_file: Path, checksum: str, expectation: ContextManager) -> None:
     """Tests the 'download.get_checksums() function"""
 
     with expectation:
         md5_input_path = data_dir / checksum_file
         obtained_checksums = get_checksums(md5_input_path)
-        assert obtained_checksums["annotation_hashes.txt"] == "40df91d5c40cb55621c4c92201da6834"
+        assert obtained_checksums.get("annotation_hashes.txt", None) == checksum
+
+#################
+@pytest.mark.dependency(name="test_md5_files")
+@pytest.mark.parametrize(
+    "md5file, checksum_bool",
+    [
+        ("md5checksums.txt", True),  # Normal case
+        ("wrong_md5_checksums.txt", False),  # Bad md5sum file (in correct md5)
+        (None, True),  # No md5file specified, resort to default
+        (Path("*"), False),  # Pass checksum_file as incompatible os path '*'
+        ("missingfile_md5.txt", False),  # Pass md5_checksum file with reference to missing file
+    ],
+)
+@pytest.mark.dependency(depends=["test_checksums"])
+def test_md5_files(data_dir: Path, md5file: Path, checksum_bool: bool) -> None:
+    """Tests the md5_files() function"""
+    return_bool_on_md5files = md5_files(data_dir, md5file)
+    assert return_bool_on_md5files == checksum_bool
+
+#################
+
+
