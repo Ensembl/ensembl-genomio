d6a476d3c3348c799400bbb3c98bd684bd0a78e3
Matthieu Barba <mbarba@ebi.ac.uk>
2024-02-14 16:03:20 +0000

    Add actual test db in sqlite

--- src/python/tests/database/test_dbconnection_lite.py
+++ src/python/tests/database/test_dbconnection_lite.py
@@ -15,33 +15,60 @@
 """Unit testing of `ensembl.io.genomio.database.dbconnection_lite` module.
 """
 
-from typing import List
-
+from pathlib import Path
+import pytest
 from unittest.mock import patch
+
+from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+from sqlalchemy.orm import Session
 
 from ensembl.io.genomio.database import DBConnectionLite
+from ensembl.core.models import Base, Meta
+
+metadata_content = {
+    "species": ["Lorem Ipsum"],
+    "classification": ["Insecta", "Lorem"],
+}
+
+# Create a database with only a meta table
+@pytest.fixture(name="db_file", scope='session')
+def db_file_test(tmp_dir: Path) -> Path:
+    """Get a path to a db file."""
+    test_db_file = tmp_dir / "tmp_sqlite.db"
+    return test_db_file
+
+@pytest.fixture(name="db_engine", scope='session')
+def db_engine_test(db_file: Path):
+    """Get a SQLalchemy engine to a populated database."""
+    db_url = f"sqlite:///{db_file}"
+    test_db_engine = create_engine(db_url)
+    Base.metadata.tables["meta"].create(test_db_engine)
+
+    # Add some basic data
+    with Session(test_db_engine) as session:
+        metas = []
+        for meta_key, meta_values in metadata_content.items():
+            for meta_value in meta_values:
+                metas.append(Meta(meta_key=meta_key, meta_value=meta_value))
+        session.add_all(metas)
+        session.commit()
+
+    return test_db_engine
 
 
-class MockURL:
-    """Mocker of URL class."""
-
-    def __init__(self, db_name: str):
-        self.database = db_name
-
-
-class MockEngine:
-    """Mocker of `sqlalchemy.engine.Engine` class."""
-
-    def __init__(self, db_name: str) -> None:
-        self.url = MockURL(db_name)
-
-
+@pytest.fixture(name="dbc", scope="session")
 @patch("ensembl.io.genomio.database.dbconnection_lite.create_engine")
-def test_db_name(mock_create_engine) -> None:
-    db_name = "testdb"
-    mock_create_engine.return_value = MockEngine(db_name)
-
-    test_url = URL.create("sqlite")
+def dbc_test(mock_create_engine, db_file: Path, db_engine) -> None:
+    mock_create_engine.return_value = db_engine
+    test_url = URL.create(f"sqlite://{db_file}")
     dbc = DBConnectionLite(test_url)
-    assert dbc.db_name == db_name
+    return dbc
+
+def test_db_name(dbc, db_file: Path) -> None:
+    assert Path(dbc.db_name) == Path(db_file)
+
+
+def test_load_metadata(dbc) -> None:
+    dbc._load_metadata()
+    assert dbc._metadata == metadata_content
