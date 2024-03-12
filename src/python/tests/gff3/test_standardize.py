c451c2a0bcd1b8954f12572c9fdc12b01cf7fd2b
Matthieu Barba <mbarba@ebi.ac.uk>
2024-03-12 12:35:36 +0000

    Add a structure generator

--- src/python/tests/gff3/test_standardize.py
+++ src/python/tests/gff3/test_standardize.py
@@ -16,7 +16,7 @@
 
 from collections import Counter
 from contextlib import nullcontext as does_not_raise
-from typing import ContextManager, Dict, List, Optional
+from typing import Any, ContextManager, Dict, List, Optional
 
 from Bio.SeqFeature import SeqFeature, SimpleLocation
 import pytest
@@ -52,7 +52,7 @@ class FeatGenerator:
     region = "LOREM"
     source = "Foo"
 
-    def make(self, ftype: str, number: int) -> List[SeqFeature]:
+    def make(self, ftype: str, number: int = 1) -> List[SeqFeature]:
         """Create a defined bnumber of features of a given type."""
         feats = []
         for _ in range(0, number):
@@ -63,10 +63,38 @@ class FeatGenerator:
             feats.append(feat)
         return feats
 
-    def append(self, feat: SeqFeature, ftype: str, number: int) -> None:
+    def append(self, feat: SeqFeature, ftype: str, number: int = 1) -> None:
         """Create a defined bnumber of features of a given type and append them to the gene."""
         subs = self.make(ftype, number)
         feat.sub_features += subs
+    
+    def append_structure(self, parent: SeqFeature, children: List[Any]) -> None:
+        """Load a structure in the form:
+        struct = ["mRNA"]
+        struct = [{"mRNA": ["CDS"]}]
+        struct = [{"mRNA": ["CDS", "exon"]}, "exon", "exon"]
+        """
+        for child in children:
+            if type(child) is str:
+                self.append(parent, child)
+            elif type(child) is dict:
+                feat_type = list(child.keys())[0]
+                feat_children = list(child.values())[0]
+
+                feat = self.make(feat_type)[0]
+                parent.sub_features.append(feat)
+
+                for feat_child in feat_children:
+                    self.append(feat, feat_child)
+    
+    def get_sub_structure(self, feat: SeqFeature) -> List[Any]:
+        if feat.sub_features:
+            feat_subs = []
+            for sub in feat.sub_features:
+                feat_subs.append(self.get_sub_structure(sub))
+            return {feat.type: feat_subs}
+        else:
+            return feat.type
 
 
 @pytest.mark.parametrize(
@@ -206,18 +234,28 @@ def test_move_cds_to_existing_mrna(
             assert fcounter_mrna == expected_mrna_children
 
 
-def test_remove_extra_exons():
+@pytest.mark.parametrize(
+    "children, has_id, expected_children, expectation",
+    [
+        param(["mRNA"], False, ["mRNA"], does_not_raise(), id="mRNA only, skip"),
+        param(["mRNA", "exon"], False, ["mRNA", "exon"], does_not_raise(), id="mRNA and 1 exon without id"),
+        param(["mRNA", "exon"], True, ["mRNA"], does_not_raise(), id="mRNA and 1 exon with id"),
+        param([{"mRNA": ["exon"]}, "exon"], True, [{"mRNA": ["exon"]}], does_not_raise(), id="mRNA and 1 exon with id"),
+        param([{"mRNA": ["exon"]}, "exon"], False, [{"mRNA": ["exon"]}, "exon"], does_not_raise(), id="mRNA and 1 exon without id"),
+    ]
+)
+def test_remove_extra_exons(children: List[Any], has_id: bool, expected_children: List[Any], expectation: ContextManager):
     """Test the addition of intermediate transcripts."""
     gen = FeatGenerator()
     gene = gen.make("gene", 1)[0]
+    gen.append_structure(gene, children)
 
-    gen.append(gene, "mRNA", 1)
-    gen.append(gene, "exon", 2)
-    exon_num = 1
-    for subfeat in gene.sub_features:
-        if subfeat.type == "exon":
-            subfeat.id = f"id-{exon_num}"
-            exon_num += 1
+    if has_id:
+        exon_num = 1
+        for subfeat in gene.sub_features:
+            if subfeat.type == "exon":
+                subfeat.id = f"id-{exon_num}"
+                exon_num += 1
 
     remove_extra_exons(gene)
-    assert len(gene.sub_features) == 1
+    assert gen.get_sub_structure(gene) == {"gene": expected_children}
