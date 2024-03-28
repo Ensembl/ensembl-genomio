14274d315b4ba5cc17ab9e80ee773ba2ef1158ad
Matthieu Barba <mbarba@ebi.ac.uk>
2024-03-28 09:36:57 +0000

    Ignore too many branches for now

--- src/python/ensembl/io/genomio/manifest/check_integrity.py
+++ src/python/ensembl/io/genomio/manifest/check_integrity.py
@@ -154,7 +154,7 @@ class Manifest:
             if readable_hash != md5sum:
                 raise InvalidIntegrityError(f"Invalid md5 checksum for {file_path}")
 
-    def prepare_integrity_data(self) -> None:
+    def prepare_integrity_data(self) -> None:  # pylint: disable=too-many-branches
         """Read all the files and keep a record (IDs and their lengths)
         for each cases to be compared later.
         """
