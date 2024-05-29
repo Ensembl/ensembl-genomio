55df031ef690bf510070d8d0fe924abc8f7f2341
Lahcen Campbell <lcampbell@ebi.ac.uk>
2024-05-29 13:52:21 +0100

    Another refactor to obtain binaries

--- containers/docker/datasets-cli/get_datasets_latest.sh
+++ containers/docker/datasets-cli/get_datasets_latest.sh
@@ -2,9 +2,15 @@
 
 # Get the latest version of NCBI's datasets-cli software
 DS_SOFTWARE_URL="https://api.github.com/repos/ncbi/datasets/releases/latest"
-DATASETS_LATEST=`curl -s $DS_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | egrep linux-amd64.cli.package.zip`
+DATASETS_LATEST=`curl -s $DS_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | grep linux-amd64.cli.package.zip`
 DATASETS_RELEASE=`echo "$DATASETS_LATEST" | cut -d "/" -f 8`
 
 JQ_SOFTWARE_URL="https://api.github.com/repos/jqlang/jq/releases/latest"
-JQ_LATEST=`curl -s $JQ_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | egrep jq-linux-amd64`
-echo -e -n "$DATASETS_LATEST\n$DATASETS_RELEASE\n$JQ_LATEST\n" > docker_env_vars.tmp
+JQ_LATEST=`curl -s $JQ_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | grep jq-linux-amd64`
+
+wget -q $DATASETS_LATEST
+unzip linux-amd64.cli.package.zip
+rm linux-amd64.cli.package.zip
+chmod +x datasets dataformat
+wget $JQ_LATEST -O jq
+chmod +x jq
