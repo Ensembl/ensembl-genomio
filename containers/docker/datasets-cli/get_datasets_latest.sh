7969154054086bb39eacce42d81276a707178ad5
Lahcen Campbell <lcampbell@ebi.ac.uk>
2024-05-29 10:41:42 +0100

    Remove software version info script

--- containers/docker/datasets-cli/get_datasets_latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-# Get the latest version of NCBI's datasets-cli software
-DS_SOFTWARE_URL="https://api.github.com/repos/ncbi/datasets/releases/latest"
-export DATASETS_LATEST=`curl -s $DS_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | egrep linux-amd64.cli.package.zip`
-export DATASETS_RELEASE=`echo "$DATASETS_LATEST" | cut -d "/" -f 8`
-
-JQ_SOFTWARE_URL="https://api.github.com/repos/jqlang/jq/releases/latest"
-export JQ_LATEST=`curl -s $JQ_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | egrep jq-linux-amd64`
