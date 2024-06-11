a9c91d2a057b7598404bb9e18324d781d31873b7
Lahcen Campbell <lcampbell@ebi.ac.uk>
2024-06-11 15:45:33 +0100

    Attempt to dynamically define dataset version within dockerfile

--- containers/docker/datasets-cli/get_datasets_latest.sh
+++ containers/docker/datasets-cli/get_datasets_latest.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 # Get the latest version of NCBI's datasets-cli software
 DS_SOFTWARE_URL="https://api.github.com/repos/ncbi/datasets/releases/latest"
-DATASETS_LATEST=`curl -s $DS_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | grep linux-amd64.cli.package.zip`
-DATASETS_RELEASE=`echo "$DATASETS_LATEST" | cut -d "/" -f 8`
 
+DATASETS_LATEST=`curl -s $DS_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | grep linux-amd64.cli.package.zip`
+
+DATASETS_RELEASE=`echo "$DATASETS_LATEST" | cut -d "/" -f 8`
+export DATASETS_RELEASE
+
+# Get the latest 'jq' software
 JQ_SOFTWARE_URL="https://api.github.com/repos/jqlang/jq/releases/latest"
 JQ_LATEST=`curl -s $JQ_SOFTWARE_URL | grep browser_download_url | cut -d \" -f4 | grep jq-linux-amd64`
 
