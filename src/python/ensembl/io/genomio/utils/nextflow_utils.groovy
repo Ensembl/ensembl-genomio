108d172449e33b2afaea4bbc07baa3958741790b
dishalodha <dishalodha@ebi.ac.uk>
2024-06-13 18:56:45 +0100

    create a seperate utils for nextflow

---
+++ src/python/ensembl/io/genomio/utils/nextflow_utils.groovy
@@ -0,0 +1,36 @@
+// See the NOTICE file distributed with this work for additional information
+//regarding copyright ownership.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+"""Utils to deal with nextflow parameters."""
+
+def Param_multi_values(String multi_value = "", List allowed_values, Boolean run_all = false) {
+
+    if (multi_value.isEmpty()){
+        return []
+    }
+    else {
+        all_params = multi_value.tokenize(',')
+        all_params.each { value ->
+            if (!allowed_values.contains(value)) {
+                throw new Exception("Invalid param value: $value. 
+                    Allowed values are: $allowedValues")
+        }
+    }
+    }
+    if (multi_value && run_all == 'all') {
+        return allowed_values 
+    }
+    return all_params
+}
+
