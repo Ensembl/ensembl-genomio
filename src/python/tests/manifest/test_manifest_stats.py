ea0c7a3f55714649a2275278bd238efe0b3c4a6e
Matthieu Barba <mbarba@ebi.ac.uk>
2024-08-21 16:46:43 +0100

    add a test for has_length

--- src/python/tests/manifest/test_manifest_stats.py
+++ src/python/tests/manifest/test_manifest_stats.py
@@ -19,10 +19,21 @@ from pathlib import Path
 from typing import ContextManager
 
 import pytest
+from pytest import raises
 
 from ensembl.io.genomio.manifest.manifest_stats import ManifestStats, InvalidIntegrityError
 
 
-def test_manifest_stats(data_dir: Path) -> None:
+def test_manifest_stats_init(data_dir: Path) -> None:
     """Tests `Manifest_stats.__init__()`."""
-    manifest = ManifestStats(data_dir / "full_data/manifest.json")
+    manifest_stats = ManifestStats(data_dir / "full_data/manifest.json")
+    assert manifest_stats
+
+
+def test_has_length(data_dir: Path):
+    stats = ManifestStats(data_dir / "full_data/manifest.json")
+    with raises(KeyError):
+        stats.has_lengths("foobar")
+    assert not stats.has_lengths("ann_genes")
+    stats.get_functional_annotation(data_dir / "full_data" / "functional_annotation.json")
+    assert stats.has_lengths("ann_genes")
