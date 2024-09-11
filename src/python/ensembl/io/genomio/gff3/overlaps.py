2996a2cdda4377f9a93cba6e0023a7b1f010c48f
Lahcen Campbell <32962169+ens-LCampbell@users.noreply.github.com>
2024-09-11 09:31:01 +0100

    Update src/python/ensembl/io/genomio/gff3/overlaps.py
    Improve help doc on input
    
    Co-authored-by: J. Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- src/python/ensembl/io/genomio/gff3/overlaps.py
+++ src/python/ensembl/io/genomio/gff3/overlaps.py
@@ -195,7 +195,7 @@ def main() -> None:
 
     for subparser in [gff3_stats_parser, overlaps_parser]:
         subparser.add_argument_src_path(
-            "--input_gff", required=True, help="Path of GFF3 file to scan feature stats or overlaps."
+            "--input_gff", required=True, help="Path of GFF3 file to process."
         )
         subparser.add_log_arguments(add_log_file=True)
 
