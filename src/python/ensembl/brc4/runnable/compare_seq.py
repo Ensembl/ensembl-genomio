0d8432d2501232c338dba1bbb303c2dc2aa83c56
dishalodha <dishalodha@ebi.ac.uk>
2024-12-18 15:17:09 +0000

    clean the file

--- src/python/ensembl/brc4/runnable/compare_seq.py
+++ src/python/ensembl/brc4/runnable/compare_seq.py
@@ -1,19 +1,4 @@
-#!env python3
-# See the NOTICE file distributed with this work for additional information
-# regarding copyright ownership.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import argparse
 import logging
 import hashlib
 from pathlib import Path
@@ -133,90 +118,6 @@ class compare_fasta():
 
                 for seq_2, name2 in only2.items():
                     len2 = len(seq_2)
-                    seq2_N = class compare_fasta():
-
-    def get_fasta(self, fasta_path: str) -> dict:
-        """Reads a fasta file and returns a dictionary of sequences."""
-        print(f"Read file {fasta_path}")
-        sequences = {}
-        with open_gz_file(fasta_path) as fasta_fh:
-            for rec in SeqIO.parse(fasta_fh, "fasta"):
-                name = rec.id
-                sequences[name] = re.sub(r"[^CGTA]", "N", str(rec.seq.upper()))
-        return sequences
-    
-    def build_seq_dict(self, seqs: dict) -> dict:
-        """Builds a dictionary of sequences, taking duplicates into account."""
-        seqs_dict = dict()
-        for name, seq in seqs.items():
-            if seq in seqs_dict:
-                seqs_dict[seq].add_id(name)
-            else:
-                seqs_dict[seq] = SeqGroup(seq, name)
-        return seqs_dict
-    
-    def find_common_groups(self, seqs1: dict, seqs2: dict) -> Tuple[dict, List[Any]]:
-        """Finds common groups of sequences between two dictionaries."""
-        comp = []
-        common = {}
-        for seq1, group1 in seqs1.items():
-            if seq1 in seqs2:
-                group2 = seqs2[seq1]
-                if group1.count == group2.count:
-                    if group1.count == 1:
-                        print(f"contains only 1 seq")
-                        common[group1.ids[0]] = group2.ids[0]
-                    else:
-                        comp.append(f"Matched 2 identical groups of sequences: {group1} and {group2}")
-                        possible_id2 = " OR ".join(group2.ids)
-                        for id1 in group1.ids:
-                            common[id1] = possible_id2
-                else:
-                    comp.append(
-                        f"Matched 2 different groups of sequences ({group1.count} vs {group2.count}): {group1} and {group2}"
-                    )
-        return common, comp
-    
-    def compare_seqs (self, fasta1: str, fasta2: str) -> bool:
-        """Todo"""
-        seq1 = self.get_fasta(fasta1)
-        seq2 = self.get_fasta(fasta2)
-
-        diff = abs(len(seq1) - len(seq2))
-        comp = []
-
-        # Compare sequences
-        seq1_dict = self.build_seq_dict(seq1)
-        seq2_dict = self.build_seq_dict(seq2)
-
-        # Compare number of sequences
-        if len(seq1) != len(seq2):
-            comp.append(f"WARNING: Different number of sequences: {len(seq1)} vs {len(seq2)}")
-
-        
-        common, group_comp = self.find_common_groups(seq1_dict, seq2_dict)
-        comp += group_comp
-
-        print("common: Contains mappings of IDs from seqs1 to either exact matches or possible matches in seqs2.", common)
-
-        # Sequences that are not common
-        only1 = {seq: group for seq, group in seq1_dict.items() if not seq in seq2_dict}
-
-        only2 = {seq: group for seq, group in seq2_dict.items() if not seq in seq1_dict}
-
-        self.check_for_Ns(only1, only2)
-        return only1, only2, common
-    
-    def check_for_Ns(only1, only2, comp):
-        names_length = {}
-        # sequences which have extra N at the end
-        if only1 and only2:
-            for seq_1, name1 in only1.items():
-                len1 = len(seq_1)
-                seq1_N = seq_1.count("N")
-
-                for seq_2, name2 in only2.items():
-                    len2 = len(seq_2)
                     seq2_N = seq_2.count("N")
 
                     if seq_2[:len1] == seq_1:
@@ -247,7 +148,6 @@ class compare_fasta():
 def parse_args(arg_list: list[str] | None) -> argparse.Namespace:
     """TODO
     Args:
-        
     arg_list: TODO
     """
     parser = ArgumentParser(description="Compare sequences between two genomes")
@@ -257,7 +157,7 @@ def parse_args(arg_list: list[str] | None) -> argparse.Namespace:
     # Add flags
     parser.add_argument(
         "--compare_seq_region",
-        action="store_true",
+            action="store_true",
         help="Enable BRC mode, i.e. use organism_abbrev for species, component for division",
     )
     parser.add_log_arguments()
@@ -265,12 +165,11 @@ def parse_args(arg_list: list[str] | None) -> argparse.Namespace:
 
 def main(arg_list: list[str] | None = None) -> None:
     """Main script entry-point.
-
     Args:
-        arg_list: TODO
-
-    """
+    arg_list: TODO
+     """
     args = parse_args(arg_list)
 
     compare_fasta.compare_seqs(args.fasta1, args.fasta2, args.compare_seq_region)
        
+
