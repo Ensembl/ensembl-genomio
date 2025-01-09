324fbe31b4dc21a97f9c9c83fd3edf566e188e8b
dishalodha <dishalodha@ebi.ac.uk>
2025-01-09 15:49:10 +0000

    run black and mypy

--- src/python/tests/fasta/test_compare.py
+++ src/python/tests/fasta/test_compare.py
@@ -14,23 +14,22 @@
 # limitations under the License.
 """Unit testing of `ensembl.io.genomio.fasta.compare` module."""
 # pylint: disable=too-many-positional-arguments
+
+import pytest
 from pathlib import Path
-from deepdiff import DeepDiff
+from io import StringIO
+
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from Bio.SeqIO import parse
-from io import StringIO
-import pytest
-from unittest.mock import MagicMock, patch
+from deepdiff import DeepDiff
+
 from pytest import TempPathFactory
+from unittest.mock import Mock, patch
 
-from ensembl.utils.archive import open_gz_file
-
-from ensembl.io.genomio.fasta.compare import CompareFasta
-from ensembl.io.genomio.fasta.compare import SeqGroup
+from ensembl.io.genomio.fasta.compare import CompareFasta, SeqGroup
 from ensembl.io.genomio.fasta import compare
 
-class TestWriteFormattedFiles:
+class TestCompareFastaFiles:
     """Test if all the expected output files are generated and formatted correctly"""
 
     fasta1_path = "fasta1"
@@ -40,7 +39,7 @@ class TestWriteFormattedFiles:
     @pytest.fixture(scope="class", autouse=True)
     def compare_fasta_instance(self) -> CompareFasta:
         """Fixture to provide a CompareFasta instance for testing."""
-        return CompareFasta(self.fasta1_path, self.fasta2_path, self.output_path)
+        return CompareFasta(Path(self.fasta1_path), Path(self.fasta2_path), self.output_path)
 
     @pytest.mark.parametrize(
         "seq_dict, expected",
@@ -48,21 +47,21 @@ class TestWriteFormattedFiles:
             pytest.param(
                 {"seq1": "ATCG", "seq2": "ATCG"},
                 {"ATCG": ["seq1", "seq2"]},
-                id="id: seq -> ids added as value when seq match"
+                id="id: seq -> ids added as value when seq match",
             ),
             pytest.param(
                 {"seq1": "ATCG", None: "ATATCG"},
                 {"ATCG": ["seq1"], "ATATCG": ["None"]},
-                id="id: seq -> ids added as value including None"
+                id="id: seq -> ids added as value including None",
             ),
             pytest.param(
                 {"seq1": "ATTCG", "seq2": "AATCG"},
                 {"ATTCG": ["seq1"], "AATCG": ["seq2"]},
-                id="id: seq -> ids added as value when no sequences match"
+                id="id: seq -> ids added as value when no sequences match",
             ),
         ],
     )
-    def test_build_seq_dict(self, compare_fasta_instance, seq_dict: dict, expected: dict) -> None:
+    def test_build_seq_dict(self, compare_fasta_instance : CompareFasta, expected: dict, seq_dict: dict) -> None:
         """Tests the `build_seq_dict` function.
 
         Args:
@@ -74,112 +73,116 @@ class TestWriteFormattedFiles:
         result = compare_fasta_instance.build_seq_dict(seq_dict)
 
         # Convert `SeqGroup` objects to simple lists for comparison
-        result_simple = {seq : seq_ids.ids for seq, seq_ids in result.items()}
+        result_simple = {seq: seq_ids.ids for seq, seq_ids in result.items()}
 
         assert result_simple == expected
-    
+
     @patch("ensembl.io.genomio.fasta.compare.SeqIO.parse")
     @patch("ensembl.io.genomio.fasta.compare.open_gz_file")
-    def test_read_fasta(self, mock_open_gz_file, mock_seqio_parse, compare_fasta_instance):
+    def test_read_fasta(self, mock_open_gz_file : Mock , mock_seqio_parse : Mock, compare_fasta_instance : CompareFasta) -> None:
         """
         Tests the `read_fasta` method of CompareFasta.
-    
+
         This test ensures that `read_fasta` correctly parses a FASTA file,
         replaces non-CGTA characters with 'N', and returns the expected dictionary.
         """
-    
+
         # Define the mock FASTA file path
-        fasta_path = Path("dummy1.fasta")
-    
+        fasta_path = Path(self.fasta1_path)
+
         # Create mock SeqRecord objects
         seq_record1 = SeqRecord(Seq("ATCGNNNN"), id="seq1")
         seq_record2 = SeqRecord(Seq("NANTGCGT"), id="seq2")
-    
+
         # Expected output after replacing non-CGTA characters with 'N'
         expected_output = {
             "seq1": "ATCGNNNN",
             "seq2": "NANTGCGT",
         }
-    
+
         # Configure the mock for open_gz_file to return a mock file handle
         mock_fasta_fh = StringIO(">seq1\nATCGNNNN\n>seq2\nNANTGCGT\n")
         mock_open_gz_file.return_value = mock_fasta_fh
-    
+
         # Configure the mock for SeqIO.parse to return the mock SeqRecord objects
         mock_seqio_parse.return_value = [seq_record1, seq_record2]
-    
+
         # Call the method under test
         result = compare_fasta_instance.read_fasta(fasta_path)
-    
+
         # Assertions to ensure methods were called correctly
         mock_open_gz_file.assert_called_once_with(fasta_path)
         mock_seqio_parse.assert_called_once_with(mock_fasta_fh, "fasta")
-    
+
         # Assert that the result matches the expected output
         assert result == expected_output, "The read_fasta method did not return the expected dictionary."
 
     @pytest.mark.parametrize(
-    "seq_dict1, seq_dict2, expected_common, expected_comp",
-    [
-        pytest.param(
-            # Input dictionaries with SeqGroup objects
-            {"AATCG": SeqGroup("id1")},
-            {"AATCG": SeqGroup("id2")},
-            # Expected common output
-            {"id1": "id2"},
-            # Expected comparison logs
-            [],
-            id="Matching single-ID groups"
-        ),
-        pytest.param(
-            # Multi-ID groups with identical counts
-            {"ATTCG": SeqGroup("id1"), "ATCG": SeqGroup("id4"), "ATCG": SeqGroup("id5")},
-            {"ATCG": SeqGroup("id2"), "ATTCG": SeqGroup("id3")},
-            {'id1': 'id3', 'id5': 'id2 OR id3', 'id4': 'id2 OR id3'},
-            ["Matched 2 identical groups of sequences: id5, id4 and id2, id3"],
-            id="Matching multi-ID groups with same count"
-        ),
-    ]
+        "seq_dict1, seq_dict2, expected_common, expected_comp",
+        [
+            pytest.param(
+                # Input dictionaries with SeqGroup objects
+                {"AATCG": SeqGroup("id1")},
+                {"AATCG": SeqGroup("id2")},
+                # Expected common output
+                {"id1": "id2"},
+                # Expected comparison logs
+                [],
+                id="Matching single-ID groups",
+            ),
+            pytest.param(
+                # Multi-ID groups with identical counts
+                {"ATTCG": SeqGroup("id1"), "ATCG": SeqGroup("id4"), "ATCG": SeqGroup("id5")},
+                {"ATCG": SeqGroup("id2"), "ATTCG": SeqGroup("id3")},
+                {"id1": "id3", "id5": "id2 OR id3", "id4": "id2 OR id3"},
+                ["Matched 2 identical groups of sequences: id5, id4 and id2, id3"],
+                id="Matching multi-ID groups with same count",
+            ),
+        ],
     )
-    def test_find_common_groups(self,seq_dict1, seq_dict2, expected_common, expected_comp, compare_fasta_instance) -> None:
+    def test_find_common_groups(
+        self, compare_fasta_instance : CompareFasta, expected_common : dict, expected_comp : list[str], seq_dict1 : dict, seq_dict2 : dict
+    ) -> None:
         """Tests the `find_common_groups` function."""
 
         # Add extra IDs for multi-ID cases to mimic realistic scenarios
-        if "ATCG" in seq_dict1 :
+        if "ATCG" in seq_dict1:
             seq_dict1["ATCG"].add_id("id4")
-        if "ATCG" in seq_dict2 :
+        if "ATCG" in seq_dict2:
             seq_dict2["ATCG"].add_id("id3")
 
-        common, comp = compare_fasta_instance.find_common_groups(seq_dict1,seq_dict2)
+        common, comp = compare_fasta_instance.find_common_groups(seq_dict1, seq_dict2)
         assert common == expected_common
         assert comp == expected_comp
 
     @pytest.mark.parametrize(
-    "only1, only2, expected_comp",
-    [
-        pytest.param(
-            # Input dictionaries with SeqGroup objects
-            {"AATCGN": "id1"},
-            {"AATCGNN": "id2"},
-            # Expected comparison logs
-            ["Please check extra Ns added in your fasta2 in id1 and id2"],
-            id="Matching single-ID groups with extra Ns"
-        ),
-        pytest.param(
-            # Input dictionaries with SeqGroup objects
-            {"AATCGN": "id1"},
-            {"AATCGG": "id2"},
-            # Expected comparison logs
-            ["ALERT INSERTIONS at the end or diff assembly level id1 and id2",
-            "sequences have the same length, check id1 and id2"],
-            id="Matching single-ID groups with different sequences"
-        ),
-    ]
+        "only1, only2, expected_comp",
+        [
+            pytest.param(
+                # Input dictionaries with SeqGroup objects
+                {"AATCGN": "id1"},
+                {"AATCGNN": "id2"},
+                # Expected comparison logs
+                ["Please check extra Ns added in your fasta2 in id1 and id2"],
+                id="Matching single-ID groups with extra Ns",
+            ),
+            pytest.param(
+                # Input dictionaries with SeqGroup objects
+                {"AATCGN": "id1"},
+                {"AATCGG": "id2"},
+                # Expected comparison logs
+                [
+                    "ALERT INSERTIONS at the end or diff assembly level id1 and id2",
+                    "sequences have the same length, check id1 and id2",
+                ],
+                id="Matching single-ID groups with different sequences",
+            ),
+        ],
     )
-    def test_compare_seq_for_Ns(self, only1, only2, expected_comp, compare_fasta_instance):
+    def test_compare_seq_for_Ns(self, compare_fasta_instance : CompareFasta, expected_comp : list[str], only1 : dict, only2: dict) -> None:
         """Tests the `compare_seq_for_Ns` function."""
 
-         # Clear the comp list to isolate this test
+        # Clear the comp list to isolate this test
         compare_fasta_instance.comp = []
         compare_fasta_instance.compare_seq_for_Ns(only1, only2)
         assert compare_fasta_instance.comp == expected_comp
@@ -187,28 +190,29 @@ class TestWriteFormattedFiles:
     @pytest.mark.parametrize(
         "mock_seq1, mock_seq2, mock_seq1_dict, mock_seq2_dict",
         [
-        pytest.param(
-            ["ACTG", "CGTA"],
-            ["ACTG", "CGTANN"],
-            {"ACTG": "id1", "CGTA": "id2"},
-            {"ACTG": "id3", "CGTANN": "id4"},
-            id="Case with differing sequence lengths"
-        ),
-    ],
+            pytest.param(
+                ["ACTG", "CGTA"],
+                ["ACTG", "CGTANN"],
+                {"ACTG": "id1", "CGTA": "id2"},
+                {"ACTG": "id3", "CGTANN": "id4"},
+                id="Case with differing sequence lengths",
+            ),
+        ],
     )
     @patch("ensembl.io.genomio.fasta.compare.CompareFasta.write_results")
     @patch("ensembl.io.genomio.fasta.compare.CompareFasta.build_seq_dict")
     @patch("ensembl.io.genomio.fasta.compare.CompareFasta.read_fasta")
-    def test_compare_seqs(self,
-        mock_read_fasta,
-        mock_build_seq_dict,
-        mock_write_results,
-        compare_fasta_instance,
-        mock_seq1,
-        mock_seq2,
-        mock_seq1_dict,
-        mock_seq2_dict,
-    ):
+    def test_compare_seqs(
+        self,
+        mock_read_fasta : Mock,
+        mock_build_seq_dict : Mock,
+        mock_write_results : Mock,
+        compare_fasta_instance: CompareFasta,
+        mock_seq1 : list[str],
+        mock_seq2 : list[str],
+        mock_seq1_dict : dict[str, str],
+        mock_seq2_dict : dict[str, str],
+    ) -> None:
         """Tests the `compare_seqs` function."""
 
         # Configure the mocks
@@ -226,9 +230,9 @@ class TestWriteFormattedFiles:
         # Check the comparison logs
         assert len(compare_fasta_instance.comp) > 0
 
-    def test_write_results(self, compare_fasta_instance, tmp_path_factory: TempPathFactory):
+    def test_write_results(self, compare_fasta_instance : CompareFasta, tmp_path_factory: TempPathFactory) -> None:
         """Tests the `write_results` method."""
-        
+
         temp = tmp_path_factory.mktemp("temp")
         compare_fasta_instance.output_dir = temp
         compare_fasta_instance.write_results()
@@ -236,40 +240,41 @@ class TestWriteFormattedFiles:
         assert expected_file.exists(), f"Expected file {expected_file} does not exist."
 
     @pytest.mark.parametrize(
-    "arg_list, expected",
-    [
-        pytest.param(
-            ["--fasta1_path", "genome1.fasta", "--fasta2_path", "genome2.fasta"],
-            {
-                "fasta1_path": "genome1.fasta",
-                "fasta2_path": "genome2.fasta",
-                "output_dir": Path.cwd(),
-                "compare_seq_region": False,
-            },
-            id="Default args",
-        ),
-        pytest.param(
-            [
-                "--fasta1_path", "genome1.fasta",
-                "--fasta2_path", "genome2.fasta",
-                "--output_dir", "/output/dir",
-                "--compare_seq_region",
-            ],
-            {
-                "fasta1_path": "genome1.fasta",
-                "fasta2_path": "genome2.fasta",
-                "output_dir": Path("/output/dir"),
-                "compare_seq_region": True,
-            },
-            id="Custom args",
-        ),
-    ],
+        "arg_list, expected",
+        [
+            pytest.param(
+                ["--fasta1_path", "genome1.fasta", "--fasta2_path", "genome2.fasta"],
+                {
+                    "fasta1_path": "genome1.fasta",
+                    "fasta2_path": "genome2.fasta",
+                    "output_dir": Path.cwd(),
+                    "compare_seq_region": False,
+                },
+                id="Default args",
+            ),
+            pytest.param(
+                [
+                    "--fasta1_path",
+                    "genome1.fasta",
+                    "--fasta2_path",
+                    "genome2.fasta",
+                    "--output_dir",
+                    "/output/dir",
+                    "--compare_seq_region",
+                ],
+                {
+                    "fasta1_path": "genome1.fasta",
+                    "fasta2_path": "genome2.fasta",
+                    "output_dir": Path("/output/dir"),
+                    "compare_seq_region": True,
+                },
+                id="Custom args",
+            ),
+        ],
     )
-    def test_parse_args(self,arg_list: list[str], expected: dict) -> None:
+    def test_parse_args(self, arg_list: list[str], expected: dict) -> None:
         """Tests the `parse_args` function."""
         args = compare.parse_args(arg_list)
         if args.output_dir:
             args.output_dir = Path(args.output_dir)
         assert not DeepDiff(vars(args), expected), "Parsed arguments differ from expected."
-    
-        
