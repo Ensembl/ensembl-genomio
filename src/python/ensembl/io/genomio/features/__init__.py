171e738ec5ab09b4c41c9300406a822257955568
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2026-04-20 13:51:51 +0100

    Update docstring

--- src/python/ensembl/io/genomio/features/__init__.py
+++ src/python/ensembl/io/genomio/features/__init__.py
@@ -12,6 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Repeat files processing module."""
+"""Repeat/ncRNA features processing module."""
 
 from .combine_json import *
