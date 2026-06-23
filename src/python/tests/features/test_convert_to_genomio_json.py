13c612d927ded44748037d9cc2fb811eda102c88
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2026-06-23 10:28:10 +0100

    Fix another test

--- src/python/tests/features/test_convert_to_genomio_json.py
+++ src/python/tests/features/test_convert_to_genomio_json.py
@@ -1385,7 +1385,7 @@ def test_main_passes_expected_arguments(
             "input_path": input_path,
             "repeatmasker_consensus_lib_path": consensus_lib,
             "output_path": output_path,
-            "analysis_logic_name": "repeatmask_rmlib",
+            "analysis_logic_name": "repeatmask_repbase",
             "analysis_display_label": "Repeats: Repbase",
             "analysis_description": (
                 'Repeats identified by <a rel="external" href="http://www.repeatmasker.org">RepeatMasker'
